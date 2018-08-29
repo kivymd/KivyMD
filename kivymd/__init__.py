b4a0b02a9543cb36a8adca7beca76c8266c871f7
HeaTTheatR <gorodage@gmail.com>
2018-08-29 23:39:29 +0300

    Edit version

--- kivymd/__init__.py
+++ kivymd/__init__.py
@@ -1,8 +1,11 @@
 # -*- coding: utf-8 -*-
+
 import os
+
 from kivy import Logger
-__version_info__ = (0, 1, 2)
-__version__ = '0.1.2'
+
+__version_info__ = (0, 1, 3)
+__version__ = '0.1.3'
 
 path = os.path.dirname(__file__)
 fonts_path = os.path.join(path, "fonts/")

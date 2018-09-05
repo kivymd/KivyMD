d9153e6ee35ffbce406ce7226e1413c55da72897
HeaTTheatR <gorodage@gmail.com>
2018-09-05 10:58:01 +0300

    Update

--- setup.py
+++ setup.py
@@ -16,15 +16,16 @@ setup(name='kivymd',
       version=version,
       description='Set of widgets for Kivy inspired by Google\'s Material '
                   'Design',
-      author='Andrés Rodríguez',
-      author_email='andres.rodriguez@lithersoft.com',
+      author='Andrés Rodríguez, author fork - HeaTTheatR',
+      author_email='andres.rodriguez@lithersoft.com, email author fork - kivydevelopment@gmail.com',
       url='https://gitlab.com/kivymd/KivyMD',
       packages=['kivymd'],
       package_data={
           'kivymd': ['images/*.png', 'images/*.jpg', 'images/*.atlas',
-                     'vendor/*.py',
-                     'fonts/*.ttf', 'vendor/circleLayout/*.py',
+                     'fonts/*.ttf',
+                     'vendor/*.py', 'vendor/circleLayout/*.py',
                      'vendor/circularTimePicker/*.py',
-                     'vendor/navigationdrawer/*.py']},
+                     'vendor/navigationdrawer/*.py',
+                     'toast/*.py', 'toast/kivytoast/*.py']},
       requires=['kivy']
-      )
+)

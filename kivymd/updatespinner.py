96948c11b74121e0218c69363753a17a64b05e2d
HeaTTheatR <gorodage@gmail.com>
2018-09-07 12:03:37 +0300

    Edit anchor y spinner

--- kivymd/updatespinner.py
+++ kivymd/updatespinner.py
@@ -15,6 +15,7 @@ from kivymd.theming import ThemableBehavior
 
 Builder.load_string("""
 #:import Window kivy.core.window.Window
+#:import MDSpinner kivymd.spinner.MDSpinner
 
 
 <MDUpdateSpinner>:
@@ -23,7 +24,7 @@ Builder.load_string("""
         id: body_spinner
         size_hint: None, None
         size: dp(46), dp(46)
-        y: Window.height - (root.theme_cls.standard_increment - 10)
+        y: Window.height
         pos_hint: {'center_x': .5}
         anchor_x: 'center'
         anchor_y: 'center'
@@ -53,17 +54,17 @@ class MDUpdateSpinner(ThemableBehavior, FloatLayout):
 
     def on_touch_move(self, touch):
         if touch.grab_current is self and not self._spinner_work:
-            self._step += 8
-            if self._step > 160:
+            self._step += 18
+            if self._step > dp(210):
                 self._spinner_work = True
                 self.start_anim_spinner()
                 return
-            self.ids.body_spinner.y -= 8
+            self.ids.body_spinner.y -= 18
 
     def on_touch_up(self, touch):
         if touch.grab_current is self:
             touch.ungrab(self)
-            if self._step > 160 and not self._spinner_work:
+            if self._step > dp(210) and not self._spinner_work:
                 self.start_anim_spinner()
             else:
                 self.hide_anim_spinner()
@@ -84,8 +85,7 @@ class MDUpdateSpinner(ThemableBehavior, FloatLayout):
 
     def transform_hide_anim_spinner(self):
         spinner = self.ids.body_spinner
-        Animation(size=(0, 0), opacity=0,
-                  y=Window.height - (self.theme_cls.standard_increment - 10),
+        Animation(size=(0, 0), opacity=0, y=Window.height,
                   d=.2, t='in_cubic').start(spinner)
         anim = Animation(opacity=0, d=.2, t='in_cubic')
         anim.bind(on_complete=self.set_spinner)
@@ -94,15 +94,13 @@ class MDUpdateSpinner(ThemableBehavior, FloatLayout):
     def hide_anim_spinner(self):
         if not self._spinner_work:
             spinner = self.ids.body_spinner
-            Animation(
-                y=Window.height - (self.theme_cls.standard_increment - 10),
-                d=.8, t='out_elastic').start(spinner)
+            Animation(y=Window.height, d=.8, t='out_elastic').start(spinner)
             self.reset_value()
 
     def set_spinner(self, *args):
         body_spinner = self.ids.body_spinner
         body_spinner.size = (dp(46), dp(46))
-        body_spinner.y = Window.height - (self.theme_cls.standard_increment - 10)
+        body_spinner.y = Window.height
         body_spinner.opacity = 1
         spinner = self.ids.spinner
         spinner.size = (dp(30), dp(30))

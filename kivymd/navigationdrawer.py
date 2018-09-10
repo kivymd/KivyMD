383518b146051a1f720bc3fa2f6733b21c3d46e8
HeaTTheatR <gorodage@gmail.com>
2018-09-10 12:09:06 +0300

    Added example

--- kivymd/navigationdrawer.py
+++ kivymd/navigationdrawer.py
@@ -2,10 +2,81 @@
 Navigation Drawer
 =================
 
-`Material Design spec page <https://material.io/guidelines/patterns/navigation-drawer.html>`_
+Copyright © 2010-2018 HeaTTheatR
 
-API
----
+For suggestions and questions:
+<kivydevelopment@gmail.com>
+
+This file is distributed under the terms of the same license,
+as the Kivy framework.
+
+EXAMPLE:
+
+from kivy.app import App
+from kivy.lang import Builder
+
+from kivymd.navigationdrawer import NavigationDrawerIconButton
+from kivymd.theming import ThemeManager
+from kivymd.toast import toast
+
+main_kv = """
+#:import Toolbar kivymd.toolbar.Toolbar
+#:import MDNavigationDrawer kivymd.navigationdrawer.MDNavigationDrawer
+#:import NavigationDrawerSubheader kivymd.navigationdrawer.NavigationDrawerSubheader
+
+
+<ContentNavigationDrawer@MDNavigationDrawer>:
+    drawer_logo: 'demos/kitchen_sink/assets/drawer_logo.png'
+
+    NavigationDrawerSubheader:
+        text: "Menu:"
+
+
+NavigationLayout:
+    id: nav_layout
+
+    ContentNavigationDrawer:
+        id: nav_drawer
+
+    BoxLayout:
+        orientation: 'vertical'
+
+        Toolbar:
+            id: toolbar
+            title: 'KivyMD Kitchen Sink'
+            md_bg_color: app.theme_cls.primary_color
+            background_palette: 'Primary'
+            background_hue: '500'
+            elevation: 10
+            left_action_items:
+                [['dots-vertical', lambda x: app.root.toggle_nav_drawer()]]
+
+        Widget:
+"""
+
+
+class Example(App):
+    theme_cls = ThemeManager()
+    theme_cls.primary_palette = 'Blue'
+    title = "Navigation Drawer"
+    main_widget = None
+
+    def build(self):
+        self.main_widget = Builder.load_string(main_kv)
+        return self.main_widget
+
+    def callback(self, instance, value):
+        toast("Pressed item menu %d" % value)
+
+    def on_start(self):
+        for i in range(15):
+            self.main_widget.ids.nav_drawer.add_widget(
+                NavigationDrawerIconButton(
+                    icon='checkbox-blank-circle', text="Item menu %d" % i,
+                    on_release=lambda x, y=i: self.callback(x, y)))
+
+
+Example().run()
 '''
 
 from kivy.core.window import Window

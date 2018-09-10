2c345880f312d53676f55b9ff31886acb9e2ffb7
HeaTTheatR <gorodage@gmail.com>
2018-09-10 13:02:46 +0300

    Added doc

--- kivymd/button.py
+++ kivymd/button.py
@@ -8,14 +8,128 @@ Buttons
 
 `Material Design spec, Buttons: Floating Action Button page <https://www.google.com/design/spec/components/buttons-floating-action-button.html>`_
 
-TO-DO: DOCUMENT MODULE
+Copyright © 2010-2018 HeaTTheatR
+
+For suggestions and questions:
+<kivydevelopment@gmail.com>
+
+This file is distributed under the terms of the same license,
+as the Kivy framework.
+
+EXAMPLE:
+
+from kivy.app import App
+from kivy.lang import Builder
+from kivy.factory import Factory
+
+from kivymd.theming import ThemeManager
+
+Builder.load_string("""
+#:import Toolbar kivymd.toolbar.Toolbar
+#:import MDIconButton kivymd.button.MDIconButton
+#:import MDFloatingActionButton kivymd.button.MDFloatingActionButton
+#:import MDFlatButton kivymd.button.MDFlatButton
+#:import MDRaisedButton kivymd.button.MDRaisedButton
+#:import MDRectangleFlatButton kivymd.button.MDRectangleFlatButton
+#:import MDRoundFlatButton kivymd.button.MDRoundFlatButton
+#:import MDRoundFlatIconButton kivymd.button.MDRoundFlatIconButton
+#:import MDFillRoundFlatButton kivymd.button.MDFillRoundFlatButton
+
+
+<ExampleButtons@BoxLayout>:
+    orientation: 'vertical'
+
+    Toolbar:
+        id: toolbar
+        title: app.title
+        md_bg_color: app.theme_cls.primary_color
+        background_palette: 'Primary'
+        elevation: 10
+        left_action_items: [['dots-vertical', lambda x: None]]
+
+    Screen:
+        BoxLayout:
+            size_hint_y: None
+            height: '56'
+            spacing: '10dp'
+            pos_hint: {'center_y': .9}
+
+            Widget:
+
+            MDIconButton:
+                icon: 'sd'
+
+            MDFloatingActionButton:
+                icon: 'plus'
+                opposite_colors: True
+                elevation_normal: 8
+
+            MDFloatingActionButton:
+                icon: 'check'
+                opposite_colors: True
+                elevation_normal: 8
+                md_bg_color: app.theme_cls.primary_color
+
+            MDIconButton:
+                icon: 'sd'
+                theme_text_color: 'Custom'
+                text_color: app.theme_cls.primary_color
+
+            Widget:
+
+        MDFlatButton:
+            text: 'MDFlatButton'
+            pos_hint: {'center_x': 0.5, 'center_y': .75}
+
+        MDRaisedButton:
+            text: "MDRaisedButton"
+            elevation_normal: 2
+            opposite_colors: True
+            pos_hint: {'center_x': 0.5, 'center_y': .65}
+
+        MDRectangleFlatButton:
+            text: "MDRectangleFlatButton"
+            pos_hint: {'center_x': 0.5, 'center_y': .55}
+
+        MDRectangleFlatIconButton:
+            text: "I love Python"
+            icon: "language-python"
+            pos_hint: {'center_x': 0.5, 'center_y': .45}
+
+        MDRoundFlatButton:
+            text: "I love Python"
+            icon: "language-python"
+            pos_hint: {'center_x': 0.5, 'center_y': .35}
+
+        MDRoundFlatIconButton:
+            text: "I love Python"
+            icon: "language-python"
+            pos_hint: {'center_x': 0.5, 'center_y': .25}
+
+        MDFillRoundFlatButton:
+            text: "I love Python"
+            pos_hint: {'center_x': 0.5, 'center_y': .15}
+""")
+
+
+class Example(App):
+    theme_cls = ThemeManager()
+    theme_cls.primary_palette = 'Blue'
+    title = "Example Buttons"
+    main_widget = None
+
+    def build(self):
+        return Factory.ExampleButtons()
+
+
+Example().run()
 '''
 
 from kivy.clock import Clock
 from kivy.graphics.context_instructions import Color
 from kivy.graphics.stencil_instructions import StencilPush, StencilUse, \
     StencilPop, StencilUnUse
-from kivy.graphics.vertex_instructions import Line, Ellipse, RoundedRectangle
+from kivy.graphics.vertex_instructions import Ellipse, RoundedRectangle
 from kivy.lang import Builder
 from kivy.metrics import dp
 from kivy.utils import get_color_from_hex

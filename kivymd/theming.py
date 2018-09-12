11626aaf76e96fec8cf61ef129652f5d49f0aaa9
HeaTTheatR <gorodage@gmail.com>
2018-09-12 10:22:57 +0300

    A constant has been added to check if the phone model belongs to iOS

--- kivymd/theming.py
+++ kivymd/theming.py
@@ -9,8 +9,8 @@ from kivy.properties import OptionProperty, AliasProperty, ObjectProperty, \
 from kivy.uix.widget import Widget
 from kivy.utils import get_color_from_hex
 from kivy.atlas import Atlas
-from kivymd.color_definitions import colors, text_colors
-from kivymd.material_resources import FONTS, DEVICE_TYPE
+from kivymd.color_definitions import colors
+from kivymd.material_resources import FONTS, DEVICE_TYPE, DEVICE_IOS
 from kivymd import images_path
 
 for font in FONTS:
@@ -340,6 +340,7 @@ class ThemeManager(Widget):
 class ThemableBehavior(object):
     theme_cls = ObjectProperty(None)
     opposite_colors = BooleanProperty(False)
+    device_ios = DEVICE_IOS
 
     def __init__(self, **kwargs):
         if self.theme_cls is not None:

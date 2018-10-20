119384e49621543b2f0373dc9a2258c4f142c121
macbookair <kivydevelopment@gmail.com>
2018-10-20 08:50:36 +0300

    Added check on belonging to macosx

--- kivymd/material_resources.py
+++ kivymd/material_resources.py
@@ -3,11 +3,12 @@
 from kivy import platform
 from kivy.core.window import Window
 from kivy.metrics import dp
+
 from kivymd import fonts_path
 
 # Feel free to override this const if you're designing for a device such as
 # a GNU/Linux tablet.
-DEVICE_IOS = platform == "ios"
+DEVICE_IOS = platform == "ios" or platform == "macosx"
 if platform != "android" and platform != "ios":
     DEVICE_TYPE = "desktop"
 elif Window.width >= dp(600) and Window.height >= dp(600):

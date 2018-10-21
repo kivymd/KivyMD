7934fb7416778c477e1663e9c4ba5b1f243c69fb
macbookair <kivydevelopment@gmail.com>
2018-10-21 16:04:25 +0300

    Added example

--- kivymd/textfields.py
+++ kivymd/textfields.py
@@ -1,5 +1,122 @@
 # -*- coding: utf-8 -*-
 
+'''
+textfields.py
+
+A simple manager for selecting directories and files.
+Copyright © 2010-2018 HeaTTheatR
+
+For suggestions and questions:
+<kivydevelopment@gmail.com>
+
+This file is distributed under the terms of the same license,
+as the Kivy framework.
+
+EXAMPLE:
+
+from kivy.app import App
+from kivy.lang import Builder
+from kivy.factory import Factory
+
+from kivymd.theming import ThemeManager
+
+Builder.load_string("""
+#:import Toolbar kivymd.toolbar.Toolbar
+#:import MDTextField kivymd.textfields.MDTextField
+#:import MDTextFieldClear kivymd.textfields.MDTextFieldClear
+
+
+<ExampleTextFields@BoxLayout>:
+    orientation: 'vertical'
+
+    Toolbar:
+        id: toolbar
+        title: app.title
+        md_bg_color: app.theme_cls.primary_color
+        background_palette: 'Primary'
+        elevation: 10
+        left_action_items: [['dots-vertical', lambda x: None]]
+
+    ScrollView:
+
+        BoxLayout:
+            orientation: 'vertical'
+            size_hint_y: None
+            height: self.minimum_height
+            padding: dp(48)
+            spacing: 10
+
+            MDTextField:
+                hint_text: "No helper text"
+
+            MDTextField:
+                hint_text: "Helper text on focus"
+                helper_text: "This will disappear when you click off"
+                helper_text_mode: "on_focus"
+
+            MDTextField:
+                hint_text: "Persistent helper text"
+                helper_text: "Text is always here"
+                helper_text_mode: "persistent"
+
+            MDTextField:
+                id: text_field_error
+                hint_text:
+                    "Helper text on error (Hit Enter with two characters here)"
+                helper_text: "Two is my least favorite number"
+                helper_text_mode: "on_error"
+
+            MDTextField:
+                hint_text: "Max text length = 10"
+                max_text_length: 10
+
+            MDTextField:
+                hint_text: "required = True"
+                required: True
+                helper_text_mode: "on_error"
+
+            MDTextField:
+                multiline: True
+                hint_text: "Multi-line text"
+                helper_text: "Messages are also supported here"
+                helper_text_mode: "persistent"
+
+            MDTextField:
+                hint_text: "color_mode = \'accent\'"
+                color_mode: 'accent'
+
+            MDTextField:
+                hint_text: "color_mode = \'custom\'"
+                color_mode: 'custom'
+                helper_text_mode: "on_focus"
+                helper_text:
+                    "Color is defined by \'line_color_focus\' property"
+                line_color_focus:
+                    # This is the color used by the textfield
+                    self.theme_cls.opposite_bg_normal
+
+            MDTextField:
+                hint_text: "disabled = True"
+                disabled: True
+
+            MDTextFieldClear:
+                hint_text: "Text field with clearing type"
+""")
+
+
+class Example(App):
+    theme_cls = ThemeManager()
+    theme_cls.primary_palette = 'Blue'
+    title = "Example Text Fields"
+    main_widget = None
+
+    def build(self):
+        return Factory.ExampleTextFields()
+
+
+Example().run()
+'''
+
 import sys
 
 from kivy.animation import Animation

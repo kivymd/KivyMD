f9733a79ee6a3bd320bc66968696fed8960e4b61
macbookair <kivydevelopment@gmail.com>
2018-11-06 02:04:27 +0200

    PEP8

--- kivymd/backgroundcolorbehavior.py
+++ kivymd/backgroundcolorbehavior.py
@@ -1,11 +1,11 @@
 # -*- coding: utf-8 -*-
+
 from kivy.lang import Builder
 from kivy.properties import BoundedNumericProperty, ReferenceListProperty
 from kivy.properties import OptionProperty, ListProperty
 from kivy.uix.widget import Widget
 from kivy.utils import get_color_from_hex
 from kivymd.color_definitions import text_colors
-from kivymd.theming import ThemableBehavior
 
 Builder.load_string('''
 <BackgroundColorBehavior>

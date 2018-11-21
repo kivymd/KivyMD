3319e8a9ff19cd188fc5169cf92cdf5cba9d6510
macbookair <kivydevelopment@gmail.com>
2018-11-21 20:34:13 +0300

    PEP8

--- kivymd/time_picker.py
+++ kivymd/time_picker.py
@@ -16,7 +16,7 @@ Builder.load_string("""
 
 <MDTimePicker>:
     size_hint: (None, None)
-    size: [dp(270), dp(335)+dp(95)]
+    size: [dp(270), dp(335) + dp(95)]
     pos_hint: {'center_x': .5, 'center_y': .5}
 
     canvas:
@@ -34,24 +34,30 @@ Builder.load_string("""
             rgba: self.theme_cls.bg_dark
         Ellipse:
             size: [dp(220), dp(220)]
-            pos: root.pos[0] + dp(270)/2-dp(220)/2, root.pos[1] + root.height - (dp(335)/2+dp(95)) - dp(220)/2 + dp(35)
+            pos:
+                root.pos[0] + dp(270) / 2 - dp(220) / 2, root.pos[1] + \
+                root.height - (dp(335) / 2 + dp(95)) - dp(220) / 2 + dp(35)
 
     CircularTimePicker:
         id: time_picker
-        pos: (dp(270)/2)-(self.width/2), root.height-self.height
+        pos: (dp(270) / 2) - (self.width / 2), root.height - self.height
         size_hint: [.8, .8]
         pos_hint: {'center_x': 0.5, 'center_y': 0.585}
     
     MDFlatButton:
         width: dp(32)
         id: ok_button
-        pos: root.pos[0]+root.size[0]-self.width-dp(10), root.pos[1] + dp(10)
+        pos:
+            root.pos[0] + root.size[0] - self.width - dp(10), \
+            root.pos[1] + dp(10)
         text: "OK"
         on_release: root.close_ok()
 
     MDFlatButton:
         id: cancel_button
-        pos: root.pos[0]+root.size[0]-self.width-ok_button.width-dp(10), root.pos[1] + dp(10)
+        pos:
+            root.pos[0] + root.size[0] - self.width - ok_button.width - \
+            dp(10), root.pos[1] + dp(10)
         text: "Cancel"
         on_release: root.close_cancel()
 """)
@@ -69,8 +75,9 @@ class MDTimePicker(ThemableBehavior, FloatLayout, ModalView,
         try:
             self.ids.time_picker.set_time(time)
         except AttributeError:
-            raise TypeError("MDTimePicker._set_time must receive a datetime object, not a \"" +
-                            type(time).__name__ + "\"")
+            raise TypeError(
+                "MDTimePicker._set_time must receive a datetime object, "
+                "not a \"" + type(time).__name__ + "\"")
 
     def close_cancel(self):
         self.dismiss()

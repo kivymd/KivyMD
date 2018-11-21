45b36a6a5344b29482ecedbba87bebaa5b4290aa
macbookair <kivydevelopment@gmail.com>
2018-11-21 19:57:42 +0300

    Edit version

--- kivymd/progressbar.py
+++ kivymd/progressbar.py
@@ -2,31 +2,38 @@
 
 from kivy.lang import Builder
 from kivy.properties import OptionProperty, BooleanProperty
+
 from kivymd.theming import ThemableBehavior
 from kivy.uix.progressbar import ProgressBar
-
 
 Builder.load_string('''
 <MDProgressBar>:
     canvas:
         Clear
         Color:
-            rgba:  self.theme_cls.divider_color
+            rgba: self.theme_cls.divider_color
         Rectangle:
-            size:    (self.width , dp(4)) if self.orientation == 'horizontal' else (dp(4),self.height) 
-            pos:   (self.x, self.center_y - dp(4)) if self.orientation == 'horizontal' \
+            size:
+                (self.width , dp(4)) if self.orientation == 'horizontal' \
+                else (dp(4),self.height) 
+            pos:
+                (self.x, self.center_y - dp(4)) \
+                if self.orientation == 'horizontal' \
                 else (self.center_x - dp(4),self.y)
-        
-            
         Color:
-            rgba:  self.theme_cls.primary_color
+            rgba: self.theme_cls.primary_color
         Rectangle:
-            size:     (self.width*self.value_normalized, sp(4)) if self.orientation == 'horizontal' else (sp(4), \
+            size:
+                (self.width * self.value_normalized, sp(4)) \
+                if self.orientation == 'horizontal' else (sp(4), \
                 self.height*self.value_normalized)
-            pos:    (self.width*(1-self.value_normalized)+self.x if self.reversed else self.x, self.center_y - dp(4)) \
-                if self.orientation == 'horizontal' else \
-                (self.center_x - dp(4),self.height*(1-self.value_normalized)+self.y if self.reversed else self.y)
-        
+            pos:
+                (self.width*(1 - self.value_normalized) + self.x \
+                if self.reversed else self.x, self.center_y - dp(4)) \
+                if self.orientation == 'horizontal' \
+                else (self.center_x - dp(4),self.height * \
+                (1 - self.value_normalized) + self.y if self.reversed \
+                else self.y)
 ''')
 
 
@@ -34,7 +41,8 @@ class MDProgressBar(ThemableBehavior, ProgressBar):
     reversed = BooleanProperty(False)
     ''' Reverse the direction the progressbar moves. '''
     
-    orientation = OptionProperty('horizontal', options=['horizontal', 'vertical'])
+    orientation = OptionProperty('horizontal',
+                                 options=['horizontal', 'vertical'])
     ''' Orientation of progressbar'''
             
     

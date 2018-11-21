a7d4c29efe3b6d1c973a481bec12f0de9f585423
macbookair <kivydevelopment@gmail.com>
2018-11-21 19:22:25 +0300

    PEP8

--- kivymd/theming.py
+++ kivymd/theming.py
@@ -1,4 +1,5 @@
 # -*- coding: utf-8 -*-
+
 from kivy.app import App
 from kivy.core.text import LabelBase
 from kivy.core.window import Window
@@ -9,6 +10,7 @@ from kivy.properties import OptionProperty, AliasProperty, ObjectProperty, \
 from kivy.uix.widget import Widget
 from kivy.utils import get_color_from_hex
 from kivy.atlas import Atlas
+
 from kivymd.color_definitions import colors
 from kivymd.material_resources import FONTS, DEVICE_TYPE, DEVICE_IOS
 from kivymd import images_path

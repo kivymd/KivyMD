67ba4a35582167a697329e5427e3288ddd611de6
macbookair <kivydevelopment@gmail.com>
2019-04-13 21:12:23 +0300

    Module logic completely rewritten

--- kivymd/snackbars.py
+++ kivymd/snackbars.py
@@ -15,124 +15,71 @@ as the Kivy framework.
 `Material Design spec, Snackbars <https://material.io/design/components/snackbars.html>`
 """
 
-from collections import deque
 from kivy.animation import Animation
 from kivy.clock import Clock
 from kivy.core.window import Window
-from kivy.event import EventDispatcher
 from kivy.lang import Builder
-from kivy.metrics import dp
 from kivy.properties import ObjectProperty, StringProperty, NumericProperty
-from kivy.uix.relativelayout import RelativeLayout
+from kivy.uix.floatlayout import FloatLayout
 
-from kivymd.material_resources import DEVICE_TYPE
+from kivymd.button import MDFlatButton
 
 Builder.load_string('''
-#:import Window kivy.core.window.Window
 #:import get_color_from_hex kivy.utils.get_color_from_hex
-#:import MDFlatButton kivymd.button.MDFlatButton
 #:import MDLabel kivymd.label.MDLabel
-#:import DEVICE_TYPE kivymd.material_resources.DEVICE_TYPE
 
 
-<_SnackbarWidget>
-    canvas:
-        Color:
-            rgb: get_color_from_hex('323232')
-        Rectangle:
-            size: self.size
-
-    size_hint_y: None
-    size_hint_x: 1 if DEVICE_TYPE == 'mobile' else None
-    height: dp(48) if _label.texture_size[1] < dp(30) else dp(80)
-    width: dp(24) + _label.width + _spacer.width + root.padding_right\
-        if root.button_text == '' else dp(24)\
-        + _label.width + _spacer.width + _button.width + root.padding_right
-    top: 0
-    x: 0 if DEVICE_TYPE == 'mobile' else Window.width/2 - self.width/2
+<Snackbar>:
 
     BoxLayout:
-        width:
-            Window.width - root.padding_right - _spacer.width - dp(24)\
-            if DEVICE_TYPE == 'mobile' and root.button_text == ''\
-            else Window.width - root.padding_right - _button.width\
-            - _spacer.width - dp(24)\
-            if DEVICE_TYPE == 'mobile' else _label.texture_size[0]\
-            if (dp(568) - root.padding_right - _button.width\
-            - _spacer.width - _label.texture_size[0] - dp(24)) >= 0\
-            else (dp(568) - root.padding_right - _button.width\
-            - _spacer.width - dp(24))
-        size_hint_x: None
-        x: dp(24)
+        id: box
+        size_hint_y: None
+        height: dp(58)
+        spacing: dp(5)
+        padding: dp(10)
+        y: -self.height
+
+        canvas:
+            Color:
+                rgba: get_color_from_hex('323232')
+            Rectangle:
+                pos: self.pos
+                size: self.size
 
         MDLabel:
-            id: _label
+            id: text_bar
+            size_hint_y: None
+            height: self.texture_size[1]
             text: root.text
             theme_text_color: 'Custom'
             text_color: get_color_from_hex('ffffff')
-            size: self.texture_size
-
-    BoxLayout:
-        id: _spacer
-        size_hint_x: None
-        x: _label.right
-        width: 0
-
-    MDFlatButton:
-        id: _button
-        text: root.button_text
-        theme_text_color: 'Custom'
-        text_color: get_color_from_hex('ffffff')
-        size_hint_x: None
-        x: _spacer.right if root.button_text != '' else root.right
-        center_y: root.height/2
-        on_release: root.button_callback()
+            shorten: True
+            shorten_from: 'right'
+            pos_hint: {'center_y': .5}
 ''')
 
 
-class SnackbarManager:
-    playing = False
-    queue = deque()
-
-    def _play_next(self, dying_widget=None):
-        if (dying_widget or not self.playing) and len(self.queue) > 0:
-            self.playing = True
-            self.queue.popleft().begin()
-        elif len(self.queue) == 0:
-            self.playing = False
-
-    def make(self, text, button_text=None, button_callback=None, duration=3):
-        if button_text is not None and button_callback is not None:
-            self.queue.append(_SnackbarWidget(text=text,
-                                              button_text=button_text,
-                                              button_callback=button_callback,
-                                              duration=duration))
-        else:
-            self.queue.append(_SnackbarWidget(text=text, duration=duration))
-        self._play_next()
-
-
-class Snackbar(EventDispatcher):
+class Snackbar(FloatLayout):
     """A Material Design Snackbar"""
 
-    text = StringProperty("")
+    text = StringProperty()
     """The text that will appear in the Snackbar.
 
     :attr:`text` is a :class:`~kivy.properties.StringProperty` 
     and defaults to ''.
     """
 
-    button_text = StringProperty(None, allownone=True)
+    button_text = StringProperty()
     """The text that will appear in the Snackbar's button.
 
     .. note::
         If this variable is None, the Snackbar will have no button.
 
     :attr:`button_text` is a :class:`~kivy.properties.StringProperty`
-    and defaults to None.
+    and defaults to ''.
     """
 
-    button_callback = ObjectProperty(None, allownone=True)
+    button_callback = ObjectProperty()
     """The callback that will be triggered when the Snackbar's
     button is pressed.
 
@@ -150,55 +97,29 @@ class Snackbar(EventDispatcher):
     and defaults to 3.
     """
 
-    def __init__(self, text, button_text=None, button_callback=None,
-                 duration=None):
-        self.text = text
-        self.button_text = button_text
-        self.button_callback = button_callback
-        self.duration = duration or self.duration
-        super(Snackbar, self).__init__()
+    _interval = 0
+
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        if self.button_text != '':
+            button = MDFlatButton(text=self.button_text)
+            self.ids.box.add_widget(button)
+            if self.button_callback:
+                button.bind(on_release=self.button_callback)
 
     def show(self):
-        """Show the Snackbar"""
+        """Show the Snackbar."""
 
-        manager.make(text=self.text, button_text=self.button_text,
-                     button_callback=self.button_callback,
-                     duration=self.duration)
+        def wait_interval(interval):
+            self._interval += interval
+            if self._interval > self.duration:
+                anim = Animation(y=-self.ids.box.height, d=.2)
+                anim.bind(on_complete=lambda *args: Window.parent.remove_widget(self))
+                anim.start(self.ids.box)
+                Clock.unschedule(wait_interval)
+                self._interval = 0
 
-
-class _SnackbarWidget(RelativeLayout):
-    text = StringProperty()
-    button_text = StringProperty()
-    button_callback = ObjectProperty()
-    duration = NumericProperty()
-    padding_right = NumericProperty(dp(24))
-
-    def __init__(self, text, duration, button_text='', button_callback=None,
-                 **kwargs):
-        super(_SnackbarWidget, self).__init__(**kwargs)
-        self.text = text
-        self.button_text = button_text
-        self.button_callback = button_callback
-        self.duration = duration
-        self.ids._label.text_size = (None, None)
-
-    def begin(self):
-        if self.button_text == '':
-            self.remove_widget(self.ids['_button'])
-        else:
-            self.ids._spacer.width =\
-                dp(16) if DEVICE_TYPE == "mobile" else dp(40)
-            self.padding_right = dp(16)
-        Window.add_widget(self)
-        anim = Animation(y=0, duration=.3, t='out_quad')
-        anim.start(self)
-        Clock.schedule_once(lambda dt: self.die(), self.duration)
-
-    def die(self):
-        anim = Animation(top=0, duration=.3, t='out_quad')
-        anim.bind(on_complete=lambda *args: manager._play_next(self))
-        anim.bind(on_complete=lambda *args: Window.remove_widget(self))
-        anim.start(self)
-
-
-manager = SnackbarManager()
+        Window.parent.add_widget(self)
+        anim = Animation(y=0, d=.2)
+        anim.bind(on_complete=lambda *args: Clock.schedule_interval(wait_interval, 0))
+        anim.start(self.ids.box)

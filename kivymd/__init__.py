9a9855f8881f4ef1597aa1e0afdee86a269d249e
macbookair <kivydevelopment@gmail.com>
2019-04-29 20:20:25 +0300

    Edit version

--- kivymd/__init__.py
+++ kivymd/__init__.py
@@ -18,8 +18,8 @@ import os
 
 from kivy import Logger
 
-__version_info__ = (0, 98.6)
-__version__ = '0.98.6'
+__version_info__ = (0, 98.7)
+__version__ = '0.98.7'
 
 path = os.path.dirname(__file__)
 fonts_path = os.path.join(path, "fonts/")

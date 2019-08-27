7f7bc636e2733cdf1832709f4d981df4e3d22b1f
KivyMD <HeaTTheatR@users.noreply.github.com>
2019-08-27 11:28:03 +0300

    Update setup.py
    Added `uix` to package_data

--- setup.py
+++ setup.py
@@ -38,6 +38,7 @@ setup(
             "toast/androidtoast/*.py",
             "stiffscroll/*.py",
             "utils/*.py",
+            "uix/*.py",
         ]
     },
     requires=["kivy", "pillow"],

2930c9102c22a37462d08b24c5650d9290803979
macbookair <kivydevelopment@gmail.com>
2019-10-08 16:42:46 +0300

    Updated Copyright

--- kivymd/font_definitions.py
+++ kivymd/font_definitions.py
@@ -1,23 +1,20 @@
-# Copyright (c) 2015 Andrés Rodríguez and KivyMD contributors -
-#     KivyMD library up to version 0.1.2
-# Copyright (c) 2019 Ivanov Yuri and KivyMD contributors -
-#     KivyMD library version 0.1.3 and higher
-#
-# For suggestions and questions:
-# <kivydevelopment@gmail.com>
-#
-# This file is distributed under the terms of the same license,
-# as the Kivy framework.
-
 """
 Font Definitions
 ================
 
-`Material Design spec, The type system
-<https://material.io/design/typography/the-type-system.html>`_
-"""
+Copyright (c) 2015 Andrés Rodríguez and KivyMD contributors -
+    KivyMD library up to version 0.1.2
+Copyright (c) 2019 Ivanov Yuri and KivyMD contributors -
+    KivyMD library version 0.1.3 and higher
 
-__all__ = ("fonts", "theme_font_styles")
+For suggestions and questions:
+<kivydevelopment@gmail.com>
+
+This file is distributed under the terms of the same license,
+as the Kivy framework.
+
+`Material Design spec, The type system <https://material.io/design/typography/the-type-system.html>`_
+"""
 
 from kivy.core.text import LabelBase
 from kivymd import fonts_path
@@ -50,10 +47,7 @@ fonts = [
         "fn_regular": fonts_path + "Roboto-Black.ttf",
         "fn_italic": fonts_path + "Roboto-BlackItalic.ttf",
     },
-    {
-        "name": "Icons",
-        "fn_regular": fonts_path + "materialdesignicons-webfont.ttf",
-    },
+    {"name": "Icons", "fn_regular": fonts_path + "materialdesignicons-webfont.ttf"},
 ]
 
 for font in fonts:

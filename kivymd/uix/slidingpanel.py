8ea292e238e50cca0d58cc71a2fa5f67c3939d44
macbookair <kivydevelopment@gmail.com>
2019-10-08 19:50:26 +0300

    Merged with https://github.com/HeaTTheatR/KivyMD/pull/96

--- kivymd/uix/slidingpanel.py
+++ kivymd/uix/slidingpanel.py
@@ -1,20 +1,18 @@
-# Copyright (c) 2015 Andrés Rodríguez and KivyMD contributors -
-#     KivyMD library up to version 0.1.2
-# Copyright (c) 2019 Ivanov Yuri and KivyMD contributors -
-#     KivyMD library version 0.1.3 and higher
-#
-# For suggestions and questions:
-# <kivydevelopment@gmail.com>
-#
-# This file is distributed under the terms of the same license,
-# as the Kivy framework.
-
 """
 Sliding Panel
 =============
-"""
 
-__all__ = ("SlidingPanel", "PanelShadow")
+Copyright (c) 2015 Andrés Rodríguez and KivyMD contributors -
+    KivyMD library up to version 0.1.2
+Copyright (c) 2019 Ivanov Yuri and KivyMD contributors -
+    KivyMD library version 0.1.3 and higher
+
+For suggestions and questions:
+<kivydevelopment@gmail.com>
+
+This file is distributed under the terms of the same license,
+as the Kivy framework.
+"""
 
 from kivy.animation import Animation
 from kivy.clock import Clock

4e13cb202ba265a981b21c0945082f7783715549
macbookair <kivydevelopment@gmail.com>
2019-10-08 16:39:02 +0300

    Added Copyright

--- demos/kitchen_sink/demo_apps/basedialog.py
+++ demos/kitchen_sink/demo_apps/basedialog.py
@@ -1,14 +1,12 @@
-# Copyright (c) 2019 Ivanov Yuri
-#
-# For suggestions and questions:
-# <kivydevelopment@gmail.com>
-#
-# This file is distributed under the terms of the same license,
-# as the Kivy framework.
-
 """
-Demo app: Base Dialog
-=====================
+Copyright (c) 2019 Ivanov Yuri
+
+For suggestions and questions:
+<kivydevelopment@gmail.com>
+
+This file is distributed under the terms of the same license,
+as the Kivy framework.
+
 """
 
 from kivy.lang import Builder

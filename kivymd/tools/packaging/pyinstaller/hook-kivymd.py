51e0e06b8d91c02f7b9d3a769582744f43bd6797
Artem Bulgakov <bulgakov-a-s@yandex.ru>
2019-10-20 13:34:30 +0300

    Move PyInstaller hook data to __init__

--- kivymd/tools/packaging/pyinstaller/hook-kivymd.py
+++ kivymd/tools/packaging/pyinstaller/hook-kivymd.py
@@ -1,7 +1,1 @@
-from os.path import join, basename, dirname
-import kivymd
-
-datas = [
-    (kivymd.fonts_path, join("kivymd", basename(dirname(kivymd.fonts_path)))),
-    (kivymd.images_path, join("kivymd", basename(dirname(kivymd.images_path)))),
-]
+from kivymd.tools.packaging.pyinstaller import *

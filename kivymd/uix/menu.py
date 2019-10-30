e3a3d2db152c566bda25df608aa92f5f9e4e0c13
macbookair <kivydevelopment@gmail.com>
2019-10-30 20:39:38 +0300

    Added example to doc-string

--- kivymd/uix/menu.py
+++ kivymd/uix/menu.py
@@ -14,6 +14,87 @@ This file is distributed under the terms of the same license,
 as the Kivy framework.
 
 `Material Design spec, Menus <https://material.io/design/components/menus.html>`_
+
+Example
+-------
+
+from kivy.app import App
+from kivy.lang import Builder
+from kivy.factory import Factory
+
+from kivymd.theming import ThemeManager
+from kivymd.toast import toast
+
+Builder.load_string('''
+# Here a compulsory import
+#:import MDDropdownMenu kivymd.uix.menu.MDDropdownMenu
+
+
+<Menu@Screen>
+
+    MDRaisedButton:
+        size_hint: None, None
+        size: 3 * dp(48), dp(48)
+        text: 'Open menu'
+        opposite_colors: True
+        pos_hint: {'center_x': .2, 'center_y': .9}
+        on_release: MDDropdownMenu(items=app.menu_items, width_mult=3).open(self)
+
+    MDRaisedButton:
+        size_hint: None, None
+        size: 3 * dp(48), dp(48)
+        text: 'Open menu'
+        opposite_colors: True
+        pos_hint: {'center_x': .2, 'center_y': .1}
+        on_release:
+            MDDropdownMenu(items=app.menu_items, width_mult=3).open(self)
+
+    MDRaisedButton:
+        size_hint: None, None
+        size: 3 * dp(48), dp(48)
+        text: 'Open menu'
+        opposite_colors: True
+        pos_hint: {'center_x': .8, 'center_y': .1}
+        on_release: MDDropdownMenu(items=app.menu_items, width_mult=3).open(self)
+
+    MDRaisedButton:
+        size_hint: None, None
+        size: 3 * dp(48), dp(48)
+        text: 'Open menu'
+        opposite_colors: True
+        pos_hint: {'center_x': .8, 'center_y': .9}
+        on_release: MDDropdownMenu(items=app.menu_items, width_mult=3).open(self)
+
+    MDRaisedButton:
+        size_hint: None, None
+        size: 3 * dp(48), dp(48)
+        text: 'Open menu'
+        opposite_colors: True
+        pos_hint: {'center_x': .5, 'center_y': .5}
+        on_release: MDDropdownMenu(items=app.menu_items, width_mult=4).open(self)
+''')
+
+
+class Test(App):
+    theme_cls = ThemeManager()
+    menu_items = []
+
+    def callback_for_menu_items(self, *args):
+        toast(args[0])
+
+    def build(self):
+        self.menu_items = [
+            {
+                "viewclass": "MDMenuItem",
+                "text": "Example item %d" % i,
+                "callback": self.callback_for_menu_items,
+            }
+            for i in range(15)
+        ]
+        return Factory.Menu()
+
+
+Test().run()
 """
 
 from kivy.animation import Animation

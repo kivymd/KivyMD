e08d33fa354f61e7729778d321755df03030c124
macbookair <kivydevelopment@gmail.com>
2019-12-01 13:27:41 +0300

    Updated `MDBottomAppBar` screen

--- demos/kitchen_sink/screens.py
+++ demos/kitchen_sink/screens.py
@@ -44,22 +44,92 @@ source_code_viewer = """
 """
 
 bottom_app_bar = """
+<StyleLabel@MDLabel>:
+    size_hint_y: None
+    height: self.texture_size[1]
+
+
+<StyleItemCheck@BoxLayout>:
+    group: ""
+    text: ""
+    active: False
+    size_hint_y: None
+    height: self.minimum_height
+
+    MDCheckbox:
+        group: root.group
+        active: root.active
+        size_hint: None, None
+        size: dp(48), dp(48)
+        pos_hint: {"center_y": .5}
+        on_active: app.callback_for_bottom_app_bar(root.text, self.active)
+
+    StyleLabel:
+        text: root.text
+        pos_hint: {"center_y": .5}
+
+
 <BottomAppBar@Screen>
     name: 'bottom app bar'
 
-    MDRaisedButton:
-        text: 'Anchor center'
-        pos_hint: {'center_y': .7, 'center_x': .5}
-        on_release:
-            app.md_app_bar.set_pos_action_button('center')
-            app.move_item_menu('center')
+    BoxLayout:
+        spacing: dp(10)
+        orientation: 'vertical'
+    
+        ScrollView:
+    
+            GridLayout:
+                size_hint_y: None
+                height: self.minimum_height
+                cols: 1
+                padding: "10dp"
+                spacing: "10dp"
+    
+                MDSeparator:
+        
+                StyleLabel:
+                    text: "Notch"
+                
+                StyleItemCheck:
+                    group: 'notch'
+                    text: "On"
+                    active: True
+        
+                StyleItemCheck:
+                    group: 'notch'
+                    text: "Off"
+        
+                MDSeparator:
+        
+                StyleLabel:
+                    text: "Position"
+        
+                StyleItemCheck:
+                    group: 'pos'
+                    text: "Attached - Center"
+                    active: True
+        
+                StyleItemCheck:
+                    group: 'pos'
+                    text: "Attached - End"
+    
+                StyleItemCheck:
+                    group: 'pos'
+                    text: "Free - Center"
+        
+                StyleItemCheck:
+                    group: 'pos'
+                    text: "Free - End"
 
-    MDRaisedButton:
-        text: 'Anchor right'
-        pos_hint: {'center_y': .5, 'center_x': .5}
-        on_release:
-            app.md_app_bar.set_pos_action_button('right')
-            app.move_item_menu('right')
+        MDBottomAppBar:
+        
+            MDToolbar:
+                id: bottom_toolbar
+                title: "Title"
+                icon: "git"
+                type: "bottom"
+                on_action_button: print("on_action_button")
+                left_action_items: [["menu", lambda x: x]]
 """
 
 expansion_panel = """
@@ -2615,10 +2685,7 @@ class Screens(EventDispatcher):
                 )
 
         # Actions after loading screen object
-        if name_screen == "Bottom App Bar":
-            self.set_appbar()
-            self.data[name_screen]["object"].add_widget(self.md_app_bar)
-        elif name_screen == "Text fields":
+        if name_screen == "Text fields":
             self.data[name_screen]["object"].ids.text_field_error.bind(
                 on_text_validate=self.set_error_message,
                 on_focus=self.set_error_message,

a68c06636df4fa092c9e651d81c8c7ddd930b7f5
macbookair <kivydevelopment@gmail.com>
2019-12-04 19:26:32 +0300

    Add `FitImage` class to `Factory.register`

--- kivymd/factory_registers.py
+++ kivymd/factory_registers.py
@@ -5,6 +5,7 @@ Register KivyMD widgets to use without import
 from kivy.factory import Factory
 
 r = Factory.register
+r("FitImage", module="kivymd.utils.fitimage")
 r("MDBackdrop", module="kivymd.uix.backdrop")
 r("MDTab", module="kivymd.uix.bottomnavigation")
 r("MDBanner", module="kivymd.uix.banner")

c451e71e850c3d6fd932e4ebe322791d33ef5b78
macbookair <kivydevelopment@gmail.com>
2019-12-15 19:08:33 +0300

    Added `docs` directory

---
+++ docs/build/html/_static/documentation_options.js
@@ -0,0 +1,11 @@
+var DOCUMENTATION_OPTIONS = {
+    URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
+    VERSION: 'KivyMD library up to version 0.102.1',
+    LANGUAGE: 'Python',
+    COLLAPSE_INDEX: false,
+    BUILDER: 'html',
+    FILE_SUFFIX: '.html',
+    HAS_SOURCE: true,
+    SOURCELINK_SUFFIX: '.txt',
+    NAVIGATION_WITH_KEYS: false
+};

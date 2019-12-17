ce594969502bd42bf7a8ecead726fff9994efbc0
Artem Bulgakov <bulgakov-a-s@yandex.ru>
2019-12-17 16:14:23 +0300

    Reformat Python code with Black

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -26,7 +26,7 @@ author = (
 
 # -- General configuration ---------------------------------------------------
 
-master_doc = 'index'
+master_doc = "index"
 
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom

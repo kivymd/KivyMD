8d0e96c66472c44343ef680a86b372b380df5465
macbookair <kivydevelopment@gmail.com>
2019-12-18 21:24:48 +0300

    Added `FpsMonitor` class

---
+++ kivymd/utils/fpsmonitor.py
@@ -0,0 +1,43 @@
+"""
+Monitor module
+==============
+
+The Monitor module is a toolbar that shows the activity of your current
+application :
+
+* FPS
+
+"""
+
+from kivy.clock import Clock
+from kivy.lang import Builder
+from kivy.properties import StringProperty, NumericProperty
+from kivy.uix.label import Label
+
+Builder.load_string("""
+<FpsMonitor>:
+    size_hint_y: None
+    height: self.texture_size[1]
+    text: root._fsp_value
+    pos_hint: {"top": 1}
+
+    canvas.before:
+        Color:
+            rgba: 1, 0, 1, .5
+        Rectangle:
+            pos: self.pos
+            size: self.size
+""")
+
+
+class FpsMonitor(Label):
+    updated_interval = NumericProperty(0.5)
+    """FPS refresh rate."""
+
+    _fsp_value = StringProperty()
+
+    def start(self):
+        Clock.schedule_interval(self.update_fps, self.updated_interval)
+
+    def update_fps(self, *args):
+        self._fsp_value = "FPS: %f" % Clock.get_fps()

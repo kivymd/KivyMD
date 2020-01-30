2ceef5ca96bd472194998f1580858513364caa67
macbookair <kivydevelopment@gmail.com>
2020-01-30 14:54:20 +0300

    Add `doc-string`

--- kivymd/app.py
+++ kivymd/app.py
@@ -1,16 +1,47 @@
 """
-App
-===
+MDApp
+=====
 
 This module contains :class:`MDApp` class that is inherited from
-:class:`~kivy.app.App`. :class:`MDApp` has some properties needed for KivyMD
+:class:`~kivy.app.App`. :class:`MDApp` has some properties needed for ``KivyMD``
 library (like :attr:`~MDApp.theme_cls`).
+
+You can turn on the monitor displaying the current ``FPS`` value in your application:
+
+.. code-block:: python
+
+    KV = '''
+    Screen:
+
+    MDLabel:
+        text: "Hello, World!"
+        halign: "center"
+    '''
+
+    from kivy.lang import Builder
+
+    from kivymd.app import MDApp
+
+
+    class MainApp(MDApp):
+    def build(self):
+        return Builder.load_string(KV)
+
+    def on_start(self):
+        self.fps_monitor_start()
+
+
+    MainApp().run()
+
+.. image:: https://github.com/HeaTTheatR/KivyMD-data/raw/master/gallery/kivymddoc/fps-monitor.png
+
 """
 
 __all__ = ("MDApp",)
 
 from kivy.app import App
 from kivy.properties import ObjectProperty
+
 from kivymd.theming import ThemeManager
 
 
@@ -28,3 +59,8 @@ class FpsMonitoring:
 
 class MDApp(App, FpsMonitoring):
     theme_cls = ObjectProperty(ThemeManager())
+    """
+    Instance of :class:`~ThemeManager` class.
+
+    :attr:`theme_cls` is an :class:`~kivy.properties.ObjectProperty`.
+    """

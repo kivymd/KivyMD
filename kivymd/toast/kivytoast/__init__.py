f8bb95d2625714256532a3d81ada4c80e1671762
macbookair <kivydevelopment@gmail.com>
2020-01-30 15:16:41 +0300

    Remove `Copyright`

--- kivymd/toast/kivytoast/__init__.py
+++ kivymd/toast/kivytoast/__init__.py
@@ -1,14 +1,1 @@
-"""
-KivyToast
-=========
-
-Copyright (c) 2019 Ivanov Yuri
-
-For suggestions and questions:
-<kivydevelopment@gmail.com>
-
-This file is distributed under the terms of the same license,
-as the Kivy framework.
-"""
-
 from .kivytoast import toast

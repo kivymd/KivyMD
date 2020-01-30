3aa7d00b3005a024be1b7333a04b0af32fcada80
macbookair <kivydevelopment@gmail.com>
2020-01-30 17:03:55 +0300

    Remove `Copyright`

--- kivymd/__init__.py
+++ kivymd/__init__.py
@@ -1,24 +1,3 @@
-"""
-KivyMD
-======
-
-KivyMD is a collection of Material Design compliant widgets for use with Kivy,
-a framework for cross-platform, touch-enabled graphical applications.
-The project's goal is to approximate Google's Material Design spec as close
-as possible without sacrificing ease of use or application performance.
-
-Copyright (c) 2015 Andrés Rodríguez and KivyMD contributors -
-    KivyMD library up to version 0.1.2
-Copyright (c) 2019 Ivanov Yuri and KivyMD contributors -
-    KivyMD library version 0.1.3 and higher
-
-For suggestions and questions:
-<kivydevelopment@gmail.com>
-
-This file is distributed under the terms of the same license,
-as the Kivy framework.
-"""
-
 import os
 from kivy.logger import Logger
 

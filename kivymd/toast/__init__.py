18018a96ba5ca31ef344eb32a0026f0027bb3423
macbookair <kivydevelopment@gmail.com>
2020-01-31 17:39:48 +0300

    Remove docstring from `kivymd/toast/__init__.py`

--- kivymd/toast/__init__.py
+++ kivymd/toast/__init__.py
@@ -1,11 +1,3 @@
-"""
-.. rubric:: Toast for Android device.
-
-.. image:: https://github.com/HeaTTheatR/KivyMD-data/raw/master/gallery/kivymddoc/toast.png
-
-"""
-
-
 from kivy.utils import platform
 
 

b893a0c8a5662e25263327c335023444868403ea
macbookair <kivydevelopment@gmail.com>
2020-01-31 17:49:02 +0300

    Edit docstring for `kivymd/tools/packaging/pyinstaller/__init__.py`

--- kivymd/tools/packaging/pyinstaller/__init__.py
+++ kivymd/tools/packaging/pyinstaller/__init__.py
@@ -2,49 +2,54 @@
 PyInstaller hooks
 =================
 
-Add `hookspath=[kivymd.hooks_path]` to your .spec file.
+Add ``hookspath=[kivymd.hooks_path]`` to your .spec file.
 
 Example of .spec file
 =====================
 
-# -*- mode: python ; coding: utf-8 -*-
+.. code-block:: python
 
-import sys
-import os
-from kivy_deps import sdl2, glew
-from kivymd import hooks_path as kivymd_hooks_path
+    # -*- mode: python ; coding: utf-8 -*-
 
-path = os.path.abspath(".")
+    import sys
+    import os
 
-a = Analysis(
-    ["main.py"],
-    pathex=[path],
-    hookspath=[kivymd_hooks_path],
-    win_no_prefer_redirects=False,
-    win_private_assemblies=False,
-    cipher=None,
-    noarchive=False,
-)
-pyz = PYZ(a.pure, a.zipped_data, cipher=None)
+    from kivy_deps import sdl2, glew
 
-exe = EXE(
-    pyz,
-    a.scripts,
-    a.binaries,
-    a.zipfiles,
-    a.datas,
-    *[Tree(p) for p in (sdl2.dep_bins + glew.dep_bins)],
-    debug=False,
-    strip=False,
-    upx=True,
-    name="app_name",
-    console=True,
-)
+    from kivymd import hooks_path as kivymd_hooks_path
+
+    path = os.path.abspath(".")
+
+    a = Analysis(
+        ["main.py"],
+        pathex=[path],
+        hookspath=[kivymd_hooks_path],
+        win_no_prefer_redirects=False,
+        win_private_assemblies=False,
+        cipher=None,
+        noarchive=False,
+    )
+    pyz = PYZ(a.pure, a.zipped_data, cipher=None)
+
+    exe = EXE(
+        pyz,
+        a.scripts,
+        a.binaries,
+        a.zipfiles,
+        a.datas,
+        *[Tree(p) for p in (sdl2.dep_bins + glew.dep_bins)],
+        debug=False,
+        strip=False,
+        upx=True,
+        name="app_name",
+        console=True,
+    )
 """
 
 __all__ = ("hooks_path", "datas")
 
 from os.path import dirname, abspath, join, basename
+
 import kivymd
 
 hooks_path = dirname(abspath(__file__))

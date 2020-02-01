2da5ccbe0f795546ead6fa8084ea65579dd5d9a0
macbookair <kivydevelopment@gmail.com>
2020-02-01 11:04:58 +0300

    Edit docstring for `kivymd/uix/behaviors/magic_behavior.py`

--- kivymd/uix/behaviors/magic_behavior.py
+++ kivymd/uix/behaviors/magic_behavior.py
@@ -1,38 +1,89 @@
 """
-https://gist.github.com/tshirtman/a1065cf74a788434e1162e342b130df8
-Write by tshirtman - https://github.com/tshirtman
+Magic Behavior
+==============
+
+.. rubric:: Magical effects for buttons.
+
+.. warning:: Magic effects do not work correctly with `KivyMD` buttons!
+
+To apply magic effects, you must create a new class that is inherited from the
+widget to which you apply the effect and from the :attr:`MagicBehavior` class.
+
+In `KV file`:
+
+.. code-block:: kv
+
+    <MagicButton@MagicBehavior+MDRectangleFlatButton>
+
+In `python file`:
+
+.. code-block:: python
+
+    class MagicButton(MagicBehavior, MDRectangleFlatButton):
+        pass
+
+The :attr:`MagicBehavior` class provides five effects:
+
+:attr:`MagicBehavior.wobble`
+:attr:`MagicBehavior.grow`
+:attr:`MagicBehavior.shake`
+:attr:`MagicBehavior.twist`
+:attr:`MagicBehavior.shrink`
 
 Example:
-========
 
-from kivymd.app import MDApp
-from kivy.lang import Builder
+.. code-block:: python
 
-from kivymd.theming import ThemeManager
+    from kivymd.app import MDApp
+    from kivy.lang import Builder
 
-KV = '''
-#:import MagicBehavior kivymd.uix.behaviors.MagicBehavior
-
-
-<MDIconMagicButton@MagicBehavior+MDRaisedButton>
+    KV = '''
+    #:import MagicBehavior kivymd.uix.behaviors.MagicBehavior
 
 
-FloatLayout:
-
-    MDIconMagicButton:
-        text: "GROW EFFECT"
-        on_release: self.grow()
-        pos_hint: {"center_x": .5, "center_y": .5}
-'''
+    <MagicButton@MagicBehavior+MDRectangleFlatButton>
 
 
-class Example(MDApp):
+    FloatLayout:
 
-    def build(self):
-        return Builder.load_string(KV)
+        MagicButton:
+            text: "WOBBLE EFFECT"
+            on_release: self.wobble()
+            pos_hint: {"center_x": .5, "center_y": .3}
+
+        MagicButton:
+            text: "GROW EFFECT"
+            on_release: self.grow()
+            pos_hint: {"center_x": .5, "center_y": .4}
+
+        MagicButton:
+            text: "SHAKE EFFECT"
+            on_release: self.shake()
+            pos_hint: {"center_x": .5, "center_y": .5}
+
+        MagicButton:
+            text: "TWIST EFFECT"
+            on_release: self.twist()
+            pos_hint: {"center_x": .5, "center_y": .6}
+
+        MagicButton:
+            text: "SHRINK EFFECT"
+            on_release: self.shrink()
+            pos_hint: {"center_x": .5, "center_y": .7}
+    '''
 
 
-Example().run()
+    class Example(MDApp):
+        def build(self):
+            return Builder.load_string(KV)
+
+
+    Example().run()
+
+
+.. image:: https://github.com/HeaTTheatR/KivyMD-data/raw/master/gallery/kivymddoc/magic-button.gif
+   :width: 250 px
+   :align: center
 """
 
 from kivy.animation import Animation
@@ -68,6 +119,8 @@ Builder.load_string(
 
 class MagicBehavior:
     def grow(self):
+        """Grow effect animation."""
+
         Animation.stop_all(self)
         (
             Animation(scale_x=1.2, scale_y=1.2, t="out_quad", d=0.03)
@@ -75,6 +128,8 @@ class MagicBehavior:
         ).start(self)
 
     def shake(self):
+        """Shake effect animation."""
+
         Animation.stop_all(self)
         (
             Animation(translate_x=50, t="out_quad", d=0.02)
@@ -82,6 +137,8 @@ class MagicBehavior:
         ).start(self)
 
     def wobble(self):
+        """Wobble effect animation."""
+
         Animation.stop_all(self)
         (
             (
@@ -95,6 +152,8 @@ class MagicBehavior:
         ).start(self)
 
     def twist(self):
+        """Twist effect animation."""
+
         Animation.stop_all(self)
         (
             Animation(rotate=25, t="out_quad", d=0.05)
@@ -102,6 +161,8 @@ class MagicBehavior:
         ).start(self)
 
     def shrink(self):
+        """Shrink effect animation."""
+
         Animation.stop_all(self)
         Animation(scale_x=0.95, scale_y=0.95, t="out_quad", d=0.1).start(self)
 

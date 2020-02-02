6eb3b0563a64181eef671f0b9cbc2d2922827ee7
macbookair <kivydevelopment@gmail.com>
2020-02-02 12:11:59 +0300

    Edit docstring for `kkivymd/uix/behaviors/backgroundcolorbehavior.py`

--- kivymd/uix/behaviors/backgroundcolorbehavior.py
+++ kivymd/uix/behaviors/backgroundcolorbehavior.py
@@ -2,16 +2,7 @@
 Background Color Behavior
 =========================
 
-Copyright (c) 2015 Andrés Rodríguez and KivyMD contributors -
-    KivyMD library up to version 0.1.2
-Copyright (c) 2019 Ivanov Yuri and KivyMD contributors -
-    KivyMD library version 0.1.3 and higher
-
-For suggestions and questions:
-<kivydevelopment@gmail.com>
-
-This file is distributed under the terms of the same license,
-as the Kivy framework.
+.. note:: The following classes are intended for in-house use of the library.
 """
 
 from kivy.lang import Builder
@@ -19,6 +10,7 @@ from kivy.properties import BoundedNumericProperty, ReferenceListProperty
 from kivy.properties import OptionProperty, ListProperty
 from kivy.uix.widget import Widget
 from kivy.utils import get_color_from_hex
+
 from kivymd.color_definitions import palette, hue, text_colors
 
 Builder.load_string(
@@ -36,21 +28,87 @@ Builder.load_string(
 
 class BackgroundColorBehavior(Widget):
     r = BoundedNumericProperty(1.0, min=0.0, max=1.0)
+    """The value of ``red`` in the ``rgba`` palette.
+
+    :attr:`r` is an :class:`~kivy.properties.BoundedNumericProperty`
+    and defaults to `1.0`.
+    """
+
     g = BoundedNumericProperty(1.0, min=0.0, max=1.0)
+    """The value of ``green`` in the ``rgba`` palette.
+
+    :attr:`g` is an :class:`~kivy.properties.BoundedNumericProperty`
+    and defaults to `1.0`.
+    """
+
     b = BoundedNumericProperty(1.0, min=0.0, max=1.0)
+    """The value of ``blue`` in the ``rgba`` palette.
+
+    :attr:`b` is an :class:`~kivy.properties.BoundedNumericProperty`
+    and defaults to `1.0`.
+    """
+
     a = BoundedNumericProperty(0.0, min=0.0, max=1.0)
+    """The value of ``alpha channel`` in the ``rgba`` palette.
+
+    :attr:`a` is an :class:`~kivy.properties.BoundedNumericProperty`
+    and defaults to `0.0`.
+    """
 
     md_bg_color = ReferenceListProperty(r, g, b, a)
+    """The background color of the widget (:class:`~kivy.uix.widget.Widget`)
+    that will be inherited from the :attr:`BackgroundColorBehavior` class.
+
+    For example:
+
+    .. code-block:: kv
+
+        Widget:
+            canvas:
+                Color:
+                    rgba: 0, 1, 1, 1
+                Rectangle:
+                    size: self.size
+                    pos: self.pos
+
+    similar to code:
+
+    .. code-block:: kv
+
+        <MyWidget@BackgroundColorBehavior>
+            md_bg_color: 0, 1, 1, 1
+
+    :attr:`md_bg_color` is an :class:`~kivy.properties.ReferenceListProperty`
+    and defaults to :attr:`r`, :attr:`g`, :attr:`b`, :attr:`a`.
+    """
 
 
 class SpecificBackgroundColorBehavior(BackgroundColorBehavior):
     background_palette = OptionProperty(
         "Primary", options=["Primary", "Accent", *palette]
     )
+    """See :attr:`kivymd.color_definitions.palette`.
+
+    :attr:`background_palette` is an :class:`~kivy.properties.OptionProperty`
+    and defaults to `'Primary'`.
+    """
+
     background_hue = OptionProperty("500", options=hue)
+    """See :attr:`kivymd.color_definitions.hue`.
+
+    :attr:`background_hue` is an :class:`~kivy.properties.OptionProperty`
+    and defaults to `500`.
+    """
 
     specific_text_color = ListProperty([0, 0, 0, 0.87])
+    """:attr:`specific_text_color` is an :class:`~kivy.properties.ListProperty`
+    and defaults to `[0, 0, 0, 0.87]`.
+    """
+
     specific_secondary_text_color = ListProperty([0, 0, 0, 0.87])
+    """:attr:`specific_secondary_text_color`is an :class:`~kivy.properties.ListProperty`
+    and defaults to `[0, 0, 0, 0.87]`.
+    """
 
     def _update_specific_text_color(self, instance, value):
         if hasattr(self, "theme_cls"):

101d03ec0dc2848308cfc99255ec05ce219e25cb
macbookair <kivydevelopment@gmail.com>
2020-02-20 01:29:35 +0300

    Edit docstring for `kivymd/uix/navigationdrawer.py`

--- kivymd/uix/navigationdrawer.py
+++ kivymd/uix/navigationdrawer.py
@@ -184,6 +184,87 @@ Create a menu list for ``ContentNavigationDrawer``:
 .. image:: https://github.com/HeaTTheatR/KivyMD-data/raw/master/gallery/kivymddoc/drawer-work.gif
     :align: center
 
+Switching screens in the ``ScreenManager`` and using the common ``MDToolbar``
+---------------------------------------------------------------------------
+
+.. code-block:: python
+
+    from kivy.lang import Builder
+    from kivy.uix.boxlayout import BoxLayout
+    from kivy.properties import ObjectProperty
+
+    from kivymd.app import MDApp
+
+    KV = '''
+    <ContentNavigationDrawer>:
+
+        ScrollView:
+
+            MDList:
+
+                OneLineListItem:
+                    text: "Screen 1"
+                    on_press:
+                        root.nav_drawer.set_state("close")
+                        root.screen_manager.current = "scr 1"
+
+                OneLineListItem:
+                    text: "Screen 2"
+                    on_press:
+                        root.nav_drawer.set_state("close")
+                        root.screen_manager.current = "scr 2"
+
+
+    Screen:
+
+        MDToolbar:
+            id: toolbar
+            pos_hint: {"top": 1}
+            elevation: 10
+            title: "MDNavigationDrawer"
+            left_action_items: [["menu", lambda x: nav_drawer.set_state("open")]]
+
+        NavigationLayout:
+            x: toolbar.height
+
+            ScreenManager:
+                id: screen_manager
+
+                Screen:
+                    name: "scr 1"
+
+                    MDLabel:
+                        text: "Screen 1"
+                        halign: "center"
+
+                Screen:
+                    name: "scr 2"
+
+                    MDLabel:
+                        text: "Screen 2"
+                        halign: "center"
+
+            MDNavigationDrawer:
+                id: nav_drawer
+
+                ContentNavigationDrawer:
+                    screen_manager: screen_manager
+                    nav_drawer: nav_drawer
+    '''
+
+
+    class ContentNavigationDrawer(BoxLayout):
+        screen_manager = ObjectProperty()
+        nav_drawer = ObjectProperty()
+
+
+    class TestNavigationDrawer(MDApp):
+        def build(self):
+            return Builder.load_string(KV)
+
+
+    TestNavigationDrawer().run()
+
 .. seealso::
 
     `Full example of Components-Navigation-Drawer <https://github.com/HeaTTheatR/KivyMD/wiki/Components-Navigation-Drawer>`_

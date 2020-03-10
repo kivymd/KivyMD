5ea221a46461a0d62a19750c243e9ba4dd4f4746
macbookair <kivydevelopment@gmail.com>
2020-03-10 18:37:34 +0300

    Add doc string; Add `on_open` and `on_close` events; The `core_title_text` and `core_description_text` parameters did not restore the value of `opacity` - fixed;

--- kivymd/uix/taptargetview.py
+++ kivymd/uix/taptargetview.py
@@ -1,17 +1,237 @@
 """
+Components/TapTargetView
+========================
+
 .. seealso::
 
     `TapTargetView, GitHub <https://github.com/KeepSafe/TapTargetView>`_
 
-    `TapTargetView, Material archive https://material.io/archive/guidelines/growth-communications/feature-discovery.html#>`_
+    `TapTargetView, Material archive <https://material.io/archive/guidelines/growth-communications/feature-discovery.html#>`_
 
-.. rubric:: Attempt to mimic the working of Android's TapTargetView using Kivy and Python..
+.. rubric:: Provide value and improve engagement by introducing users to new
+    features and functionality at relevant moments.
+
+.. image:: https://github.com/HeaTTheatR/KivyMD-data/raw/master/gallery/kivymddoc/tap-target-view-previous.gif
+    :align: center
+
+Usage
+-----
+
+.. code-block:: python
+
+    from kivy.lang import Builder
+
+    from kivymd.app import MDApp
+    from kivymd.uix.taptargetview import MDTapTargetView
+
+    KV = '''
+    Screen:
+
+        MDFloatingActionButton:
+            id: button
+            icon: "plus"
+            pos: 10, 10
+            on_release: app.tap_target_start()
+    '''
+
+
+    class TapTargetViewDemo(MDApp):
+        def build(self):
+            screen = Builder.load_string(KV)
+            self.tap_target_view = MDTapTargetView(
+                widget=screen.ids.button,
+                title_text="This is an add button",
+                description_text="This is a description of the button",
+                widget_position="left_bottom",
+            )
+
+            return screen
+
+        def tap_target_start(self):
+            if self.tap_target_view.state == "close":
+                self.tap_target_view.start()
+            else:
+                self.tap_target_view.stop()
+
+
+    TapTargetViewDemo().run()
+
+.. image:: https://github.com/HeaTTheatR/KivyMD-data/raw/master/gallery/kivymddoc/tap-target-view-usage.gif
+    :align: center
+
+Widget position
+---------------
+
+Sets the position of the widget relative to the floating circle.
+
+.. code-block:: python
+
+    self.tap_target_view = MDTapTargetView(
+        ...
+        widget_position="right",
+    )
+
+.. image:: https://github.com/HeaTTheatR/KivyMD-data/raw/master/gallery/kivymddoc/tap-target-view-widget-position-right.png
+    :align: center
+
+.. code-block:: python
+
+    self.tap_target_view = MDTapTargetView(
+        ...
+        widget_position="left",
+    )
+
+.. image:: https://github.com/HeaTTheatR/KivyMD-data/raw/master/gallery/kivymddoc/tap-target-view-widget-position-left.png
+    :align: center
+
+.. code-block:: python
+
+    self.tap_target_view = MDTapTargetView(
+        ...
+        widget_position="top",
+    )
+
+.. image:: https://github.com/HeaTTheatR/KivyMD-data/raw/master/gallery/kivymddoc/tap-target-view-widget-position-top.png
+    :align: center
+
+.. code-block:: python
+
+    self.tap_target_view = MDTapTargetView(
+        ...
+        widget_position="bottom",
+    )
+
+.. image:: https://github.com/HeaTTheatR/KivyMD-data/raw/master/gallery/kivymddoc/tap-target-view-widget-position-bottom.png
+    :align: center
+
+.. code-block:: python
+
+    self.tap_target_view = MDTapTargetView(
+        ...
+        widget_position="left_top",
+    )
+
+.. image:: https://github.com/HeaTTheatR/KivyMD-data/raw/master/gallery/kivymddoc/tap-target-view-widget-position-left_top.png
+    :align: center
+
+.. code-block:: python
+
+    self.tap_target_view = MDTapTargetView(
+        ...
+        widget_position="right_top",
+    )
+
+.. image:: https://github.com/HeaTTheatR/KivyMD-data/raw/master/gallery/kivymddoc/tap-target-view-widget-position-right_top.png
+    :align: center
+
+.. code-block:: python
+
+    self.tap_target_view = MDTapTargetView(
+        ...
+        widget_position="left_bottom",
+    )
+
+.. image:: https://github.com/HeaTTheatR/KivyMD-data/raw/master/gallery/kivymddoc/tap-target-view-widget-position-left_bottom.png
+    :align: center
+
+.. code-block:: python
+
+    self.tap_target_view = MDTapTargetView(
+        ...
+        widget_position="right_bottom",
+    )
+
+.. image:: https://github.com/HeaTTheatR/KivyMD-data/raw/master/gallery/kivymddoc/tap-target-view-widget-position-right_bottom.png
+    :align: center
+
+If you use ``the widget_position = "center"`` parameter then you must
+definitely specify the :attr:`~MDTapTargetView.title_position`.
+
+.. code-block:: python
+
+    self.tap_target_view = MDTapTargetView(
+        ...
+        widget_position="center",
+        title_position="left_top",
+    )
+
+.. image:: https://github.com/HeaTTheatR/KivyMD-data/raw/master/gallery/kivymddoc/tap-target-view-widget-position-center.png
+    :align: center
+
+Text options
+------------
+
+.. code-block:: python
+
+    self.tap_target_view = MDTapTargetView(
+        ...
+        title_text="Title text",
+        description_text="Description text",
+    )
+
+.. image:: https://github.com/HeaTTheatR/KivyMD-data/raw/master/gallery/kivymddoc/tap-target-view-text.png
+    :align: center
+
+
+You can use the following options to control font size, color, and boldness:
+
+- :attr:`~MDTapTargetView.title_text_size`
+- :attr:`~MDTapTargetView.title_text_color`
+- :attr:`~MDTapTargetView.title_text_bold`
+- :attr:`~MDTapTargetView.description_text_size`
+- :attr:`~MDTapTargetView.description_text_color`
+- :attr:`~MDTapTargetView.description_text_bold`
+
+.. code-block:: python
+
+    self.tap_target_view = MDTapTargetView(
+        ...
+        title_text="Title text",
+        title_text_size="36sp",
+        description_text="Description text",
+        description_text_color=[1, 0, 0, 1]
+    )
+
+.. image:: https://github.com/HeaTTheatR/KivyMD-data/raw/master/gallery/kivymddoc/tap-target-text-option.png
+    :align: center
+
+But you can also use markup to set these values.
+
+.. code-block:: python
+
+    self.tap_target_view = MDTapTargetView(
+        ...
+        title_text="[size=36]Title text[/size]",
+        description_text="[color=#ff0000ff]Description text[/color]",
+    )
+
+Events control
+--------------
+
+.. code-block:: python
+
+    self.tap_target_view.bind(on_open=self.on_open, on_close=self.on_close)
+
+.. code-block:: python
+
+    def on_open(self, instance_tap_target_view):
+        '''Called at the time of the start of the widget opening animation.'''
+
+        print("Open", instance_tap_target_view)
+
+    def on_close(self, instance_tap_target_view):
+        '''Called at the time of the start of the widget closed animation.'''
+
+        print("Close", instance_tap_target_view)
+
+.. Note:: See other parameters in the :class:`~MDTapTargetView` class.
 """
 
 from kivy.animation import Animation
 from kivy.metrics import dp
 from kivy.graphics import Color, Ellipse, Rectangle
 from kivy.event import EventDispatcher
+from kivy.logger import Logger
 from kivy.properties import (
     ObjectProperty,
     NumericProperty,
@@ -21,10 +241,18 @@ from kivy.properties import (
     OptionProperty,
 )
 from kivy.uix.label import Label
+from kivymd.theming import ThemableBehavior
 
 
-class MDTapTargetView(EventDispatcher):
-    """Rough try to mimic the working of Android's TapTargetView."""
+class MDTapTargetView(ThemableBehavior, EventDispatcher):
+    """Rough try to mimic the working of Android's TapTargetView.
+
+    :Events:
+        :attr:`on_open`
+            Called at the time of the start of the widget opening animation.
+        :attr:`on_close`
+            Called at the time of the start of the widget closed animation.
+    """
 
     widget = ObjectProperty()
     """
@@ -34,20 +262,33 @@ class MDTapTargetView(EventDispatcher):
     and defaults to `None`.
     """
 
-    outer_radius = NumericProperty(dp(300))
+    outer_radius = NumericProperty(dp(200))
     """
     Radius for outer circle.
 
+    .. image:: https://github.com/HeaTTheatR/KivyMD-data/raw/master/gallery/kivymddoc/tap-target-view-widget-outer-radius.png
+        :align: center
+
     :attr:`outer_radius` is an :class:`~kivy.properties.NumericProperty`
-    and defaults to `dp(300)`.
+    and defaults to `dp(200)`.
     """
 
-    outer_circle_color = ListProperty([1, 0, 0])
+    outer_circle_color = ListProperty()
     """
-    Color for the outer circle.
+    Color for the outer circle in ``rgb`` format.
+
+    .. code-block:: python
+
+        self.tap_target_view = MDTapTargetView(
+            ...
+            outer_circle_color=(1, 0, 0)
+        )
+
+    .. image:: https://github.com/HeaTTheatR/KivyMD-data/raw/master/gallery/kivymddoc/tap-target-view-widget-outer-circle-color.png
+        :align: center
 
     :attr:`outer_circle_color` is an :class:`~kivy.properties.ListProperty`
-    and defaults to `[1, 0, 0]`.
+    and defaults to ``theme_cls.primary_color``.
     """
 
     outer_circle_alpha = NumericProperty(0.96)
@@ -62,13 +303,26 @@ class MDTapTargetView(EventDispatcher):
     """
     Radius for target circle.
 
+    .. image:: https://github.com/HeaTTheatR/KivyMD-data/raw/master/gallery/kivymddoc/tap-target-view-widget-target-radius.png
+        :align: center
+
     :attr:`target_radius` is an :class:`~kivy.properties.NumericProperty`
     and defaults to `dp(45)`.
     """
 
     target_circle_color = ListProperty([1, 1, 1])
     """
-    Color for target circle.
+    Color for target circle in ``rgb`` format.
+
+    .. code-block:: python
+
+        self.tap_target_view = MDTapTargetView(
+            ...
+            target_circle_color=(1, 0, 0)
+        )
+
+    .. image:: https://github.com/HeaTTheatR/KivyMD-data/raw/master/gallery/kivymddoc/tap-target-view-widget-target-circle-color.png
+        :align: center
 
     :attr:`target_circle_color` is an :class:`~kivy.properties.ListProperty`
     and defaults to `[1, 1, 1]`.
@@ -169,7 +423,7 @@ class MDTapTargetView(EventDispatcher):
         ],
     )
     """
-    Sets the position of the widget on the outer_circle. Available options are
+    Sets the position of the widget on the :attr:`~outer_circle`. Available options are
     `'left`', `'right`', `'top`', `'bottom`', `'left_top`', `'right_top`',
     `'left_bottom`', `'right_bottom`', `'center`'.
 
@@ -197,6 +451,7 @@ class MDTapTargetView(EventDispatcher):
     calculates the :attr`~title_position` itself.
     Must be set to other than `'auto`' when :attr`~widget_position` is set
     to `'center`'.
+
     Available options are `'auto'`, `'left`', `'right`', `'top`', `'bottom`',
     `'left_top`', `'right_top`', `'left_bottom`', `'right_bottom`', `'center`'.
 
@@ -220,23 +475,28 @@ class MDTapTargetView(EventDispatcher):
     and defaults to `True`.
     """
 
-    end = ObjectProperty()
+    state = OptionProperty("close", options=["close", "open"])
     """
-    Function to be called when the animation stops.
+    State of :class:`~MDTapTargetView`.
 
-    :attr:`end` is an :class:`~kivy.properties.ObjectProperty`
-    and defaults to `None`.
+    :attr:`state` is an :class:`~kivy.properties.OptionProperty`
+    and defaults to `'close'`.
     """
+
+    _outer_radius = NumericProperty(0)
+    _target_radius = NumericProperty(0)
 
     def __init__(self, **kwargs):
         self.ripple_max_dist = dp(90)
-        self.outer_radius *= 2
-        self.target_radius *= 2
+        self.on_outer_radius(self, self.outer_radius)
+        self.on_target_radius(self, self.target_radius)
 
         self.core_title_text = Label(
             markup=True, size_hint=(None, None), bold=self.title_text_bold
         )
-        self.core_title_text.bind(texture_size=self.core_title_text.setter("size"))
+        self.core_title_text.bind(
+            texture_size=self.core_title_text.setter("size")
+        )
         self.core_description_text = Label(markup=True, size_hint=(None, None))
         self.core_description_text.bind(
             texture_size=self.core_description_text.setter("size")
@@ -246,10 +506,15 @@ class MDTapTargetView(EventDispatcher):
         self.register_event_type("on_outer_touch")
         self.register_event_type("on_target_touch")
         self.register_event_type("on_outside_click")
+        self.register_event_type("on_open")
+        self.register_event_type("on_close")
+
+        if not self.outer_circle_color:
+            self.outer_circle_color = self.theme_cls.primary_color[:-1]
 
     def _initialize(self):
-        setattr(self.widget, "outer_radius", 0)
-        setattr(self.widget, "target_radius", 0)
+        setattr(self.widget, "_outer_radius", 0)
+        setattr(self.widget, "_target_radius", 0)
         setattr(self.widget, "target_ripple_radius", 0)
         setattr(self.widget, "target_ripple_alpha", 0)
 
@@ -264,8 +529,12 @@ class MDTapTargetView(EventDispatcher):
 
         with self.widget.canvas.before:
             # Outer circle.
-            Color(*self.outer_circle_color, self.outer_circle_alpha, group="ttv_group")
-            _rad1 = self.widget.outer_radius
+            Color(
+                *self.outer_circle_color,
+                self.outer_circle_alpha,
+                group="ttv_group",
+            )
+            _rad1 = self.widget._outer_radius
             Ellipse(size=(_rad1, _rad1), pos=_pos[0], group="ttv_group")
 
             # Title text.
@@ -282,13 +551,16 @@ class MDTapTargetView(EventDispatcher):
             Rectangle(
                 size=self.core_description_text.texture.size,
                 texture=self.core_description_text.texture,
-                pos=(_pos[1][0], _pos[1][1] - self.core_description_text.size[1] - 5),
+                pos=(
+                    _pos[1][0],
+                    _pos[1][1] - self.core_description_text.size[1] - 5,
+                ),
                 group="ttv_group",
             )
 
             # Target circle.
             Color(*self.target_circle_color, group="ttv_group")
-            _rad2 = self.widget.target_radius
+            _rad2 = self.widget._target_radius
             Ellipse(
                 size=(_rad2, _rad2),
                 pos=(
@@ -315,16 +587,19 @@ class MDTapTargetView(EventDispatcher):
             )
 
     def stop(self, *args):
+        """Starts widget close animation."""
+
         # It needs a better implementation.
         self.anim_ripple.unbind(on_complete=self._repeat_ripple)
-        self.description_text_color = [1, 1, 1, 0]
-        self.title_text_color = [1, 1, 1, 0]
+        self.core_title_text.opacity = 0
+        self.core_description_text.opacity = 0
         anim = Animation(
             d=0.15,
             t="in_cubic",
             **dict(
                 zip(
-                    ["outer_radius", "target_radius", "target_ripple_radius"], [0, 0, 0]
+                    ["_outer_radius", "_target_radius", "target_ripple_radius"],
+                    [0, 0, 0],
                 )
             ),
         )
@@ -338,12 +613,12 @@ class MDTapTargetView(EventDispatcher):
 
         if elev:
             self._fix_elev()
-        if self.end:
-            self.end(self)
+        self.dispatch("on_close")
 
         # Don't forget to unbind the function or it'll mess
         # up with other next bindings.
         self.widget.unbind(on_touch_down=self._some_func)
+        self.state = "close"
 
     def _fix_elev(self):
         with self.widget.canvas.before:
@@ -359,12 +634,17 @@ class MDTapTargetView(EventDispatcher):
                 size=self.widget._hard_shadow_size,
                 pos=self.widget._hard_shadow_pos,
             )
-
             Color(a=1)
 
     def start(self, *args):
+        """Starts widget opening animation."""
+
         self._initialize()
         self._animate_outer()
+        self.state = "open"
+        self.core_title_text.opacity = 1
+        self.core_description_text.opacity = 1
+        self.dispatch("on_open")
 
     def _animate_outer(self):
         anim = Animation(
@@ -372,8 +652,8 @@ class MDTapTargetView(EventDispatcher):
             t="out_cubic",
             **dict(
                 zip(
-                    ["outer_radius", "target_radius"],
-                    [self.outer_radius, self.target_radius],
+                    ["_outer_radius", "_target_radius"],
+                    [self._outer_radius, self._target_radius],
                 )
             ),
         )
@@ -381,14 +661,14 @@ class MDTapTargetView(EventDispatcher):
         anim.bind(on_progress=lambda x, y, z: self._draw_canvas())
         anim.bind(on_complete=self._animate_ripple)
         anim.start(self.widget)
-        setattr(self.widget, "target_ripple_radius", self.target_radius)
+        setattr(self.widget, "target_ripple_radius", self._target_radius)
         setattr(self.widget, "target_ripple_alpha", 1)
 
     def _animate_ripple(self, *args):
         self.anim_ripple = Animation(
             d=1,
             t="in_cubic",
-            target_ripple_radius=self.target_radius + self.ripple_max_dist,
+            target_ripple_radius=self._target_radius + self.ripple_max_dist,
             target_ripple_alpha=0,
         )
         self.anim_ripple.stop_all(self.widget)
@@ -397,9 +677,20 @@ class MDTapTargetView(EventDispatcher):
         self.anim_ripple.start(self.widget)
 
     def _repeat_ripple(self, *args):
-        setattr(self.widget, "target_ripple_radius", self.target_radius)
+        setattr(self.widget, "target_ripple_radius", self._target_radius)
         setattr(self.widget, "target_ripple_alpha", 1)
         self._animate_ripple()
+
+    def on_open(self, *args):
+        """Called at the time of the start of the widget opening animation."""
+
+    def on_close(self, *args):
+        """Called at the time of the start of the widget closed animation."""
+
+    def on_draw_shadow(self, instance, value):
+        Logger.warning(
+            "The shadow adding method will be implemented in future versions"
+        )
 
     def on_description_text(self, instance, value):
         self.core_description_text.text = value
@@ -418,6 +709,12 @@ class MDTapTargetView(EventDispatcher):
 
     def on_title_text_bold(self, instance, value):
         self.core_title_text.bold = value
+
+    def on_outer_radius(self, instance, value):
+        self._outer_radius = self.outer_radius * 2
+
+    def on_target_radius(self, instance, value):
+        self._target_radius = self.target_radius * 2
 
     def on_target_touch(self):
         if self.stop_on_target_touch:
@@ -449,9 +746,9 @@ class MDTapTargetView(EventDispatcher):
         Checks if a given `pos` coordinate is within the :attr:`~outer_radius`.
         """
 
-        cx = self.circ_pos[0] + self.outer_radius / 2
-        cy = self.circ_pos[1] + self.outer_radius / 2
-        r = self.outer_radius / 2
+        cx = self.circ_pos[0] + self._outer_radius / 2
+        cy = self.circ_pos[1] + self._outer_radius / 2
+        r = self._outer_radius / 2
         h, k = pos
 
         lhs = (cx - h) ** 2 + (cy - k) ** 2
@@ -468,7 +765,7 @@ class MDTapTargetView(EventDispatcher):
 
         cx = self.widget.pos[0] + self.widget.width / 2
         cy = self.widget.pos[1] + self.widget.height / 2
-        r = self.target_radius / 2
+        r = self._target_radius / 2
         h, k = pos
 
         lhs = (cx - h) ** 2 + (cy - k) ** 2
@@ -485,7 +782,7 @@ class MDTapTargetView(EventDispatcher):
         :returns: A tuple containing pos for the circle and text.
         """
 
-        _rad1 = self.widget.outer_radius
+        _rad1 = self.widget._outer_radius
         _center_x = self.widget.x - (_rad1 / 2 - self.widget.size[0] / 2)
         _center_y = self.widget.y - (_rad1 / 2 - self.widget.size[0] / 2)
 

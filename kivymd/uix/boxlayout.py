a2c9c0565c57d6ae6643ae9b7b332cdf3f72dd04
macbookair <kivydevelopment@gmail.com>
2020-03-13 18:26:26 +0300

    Add `MDBoxLayout` class

---
+++ kivymd/uix/boxlayout.py
@@ -0,0 +1,92 @@
+"""
+Components/BoxLayout
+====================
+
+:class:`~kivy.uix.boxlayout.BoxLayout` class equivalent. Simplifies working
+with some widget properties. For example:
+
+BoxLayout
+---------
+
+.. code-block::
+
+    BoxLayout:
+        size_hint_y: None
+        height: self.minimum_height
+
+        canvas:
+            Color:
+                rgba: app.theme_cls.primary_color
+            Rectangle:
+                pos: self.pos
+                size: self.size
+
+MDBoxLayout
+-----------
+
+.. code-block::
+
+    MDBoxLayout:
+        adaptive_height: True
+        md_bg_color: app.theme_cls.primary_color
+
+Available options are:
+---------------------
+
+- adaptive_height_
+- adaptive_width_
+- adaptive_size_
+
+.. adaptive_height:
+adaptive_height
+---------------
+
+.. code-block:: kv
+
+    adaptive_height: True
+
+Equivalent
+
+.. code-block:: kv
+
+    size_hint_y: None
+    height: self.minimum_height
+
+.. adaptive_width:
+adaptive_width
+--------------
+
+.. code-block:: kv
+
+    adaptive_width: True
+
+Equivalent
+
+.. code-block:: kv
+
+    size_hint_x: None
+    height: self.minimum_width
+
+.. adaptive_size:
+adaptive_size
+-------------
+
+.. code-block:: kv
+
+    adaptive_size: True
+
+Equivalent
+
+.. code-block:: kv
+
+    size_hint: None, None
+    size: self.minimum_size
+"""
+
+from kivy.uix.boxlayout import BoxLayout
+
+from kivymd.uix import MDAdaptiveWidget
+
+
+class MDBoxLayout(BoxLayout, MDAdaptiveWidget):
+    pass

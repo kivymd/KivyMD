131626d4f8734f5792d1b85654787448d9671d0d
macbookair <kivydevelopment@gmail.com>
2020-03-15 08:13:14 +0300

    Edit doc string for kivymd/uix/list.py

--- kivymd/uix/list.py
+++ kivymd/uix/list.py
@@ -352,6 +352,49 @@ Custom list item
 
 .. image:: https://github.com/HeaTTheatR/KivyMD-data/raw/master/gallery/kivymddoc/custom-list-item.png
     :align: center
+
+.. code-block:: python
+
+    from kivy.lang import Builder
+
+    from kivymd.app import MDApp
+    from kivymd.uix.boxlayout import MDBoxLayout
+    from kivymd.uix.list import IRightBodyTouch
+
+    KV = '''
+    OneLineAvatarIconListItem:
+        text: "One-line item with avatar"
+        on_size:
+            self.ids._right_container.width = container.width
+            self.ids._right_container.x = container.width
+
+        IconLeftWidget:
+            icon: "settings"
+
+        Container:
+            id: container
+
+            MDIconButton:
+                icon: "minus"
+
+            MDIconButton:
+                icon: "plus"
+    '''
+
+
+    class Container(IRightBodyTouch, MDBoxLayout):
+        adaptive_width = True
+
+
+    class MainApp(MDApp):
+        def build(self):
+            return Builder.load_string(KV)
+
+
+    MainApp().run()
+
+.. image:: https://github.com/HeaTTheatR/KivyMD-data/raw/master/gallery/kivymddoc/custom-list-right-container.png
+    :align: center
 """
 
 from kivy.lang import Builder

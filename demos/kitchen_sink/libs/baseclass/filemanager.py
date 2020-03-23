15731444d3862170b9f5ea8bbd0e5746dc90fe56
macbookair <kivydevelopment@gmail.com>
2020-03-24 00:55:56 +0300

    Example changed in accordance with new API requirements

--- demos/kitchen_sink/libs/baseclass/filemanager.py
+++ demos/kitchen_sink/libs/baseclass/filemanager.py
@@ -1,11 +1,11 @@
 import os
 
-from kivy.uix.modalview import ModalView
 from kivy.uix.screenmanager import Screen
 
 
 class KitchenSinkFileManager(Screen):
     manager_open = False
+    file_manager = None
 
     def file_manager_open(self):
         from kivymd.uix.filemanager import MDFileManager
@@ -13,16 +13,14 @@ class KitchenSinkFileManager(Screen):
 
         def open_file_manager(text_item, dialog):
             previous = False if text_item == "List" else True
-            self.manager = ModalView(size_hint=(1, 1), auto_dismiss=False)
-            self.file_manager = MDFileManager(
-                exit_manager=self.exit_manager,
-                select_path=self.select_path,
-                previous=previous,
-            )
-            self.manager.add_widget(self.file_manager)
-            self.file_manager.show(os.environ["KITCHEN_SINK_ASSETS"])
+            if not self.file_manager:
+                self.file_manager = MDFileManager(
+                    exit_manager=self.exit_manager,
+                    select_path=self.select_path,
+                )
+            self.file_manager.previous = previous
+            self.file_manager.show(os.getcwd())
             self.manager_open = True
-            self.manager.open()
 
         MDDialog(
             title="Kitchen Sink",
@@ -49,5 +47,5 @@ class KitchenSinkFileManager(Screen):
     def exit_manager(self, *args):
         """Called when the user reaches the root of the directory tree."""
 
-        self.manager.dismiss()
         self.manager_open = False
+        self.file_manager.close()

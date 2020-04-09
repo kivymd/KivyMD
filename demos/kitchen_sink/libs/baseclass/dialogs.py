62c73f66eb18154ca4497797b6547b224aa880f2
macbookair <kivydevelopment@gmail.com>
2020-04-09 09:24:39 +0300

    Changed/added classes in accordance with the new API

--- demos/kitchen_sink/libs/baseclass/dialogs.py
+++ demos/kitchen_sink/libs/baseclass/dialogs.py
@@ -1,79 +1,120 @@
+import os
+
 from kivy.properties import ObjectProperty
+from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.screenmanager import Screen
 
+from demos.kitchen_sink.libs.baseclass.list_items import (
+    KitchenSinkOneLineLeftAvatarItem,
+)
+from kivymd.uix.button import MDFlatButton
+from kivymd.uix.dialog import MDDialog
+from kivymd.uix.list import OneLineAvatarIconListItem
 
-class KitchenSinkCDialogs(Screen):
+
+class KitchenSinkDialogsCustomContent(BoxLayout):
+    pass
+
+
+class KitchenSinkItemConfirm(OneLineAvatarIconListItem):
+    divider = None
+
+    def set_icon(self, instance_check):
+        instance_check.active = True
+        check_list = instance_check.get_widgets(instance_check.group)
+        for check in check_list:
+            if check != instance_check:
+                check.active = False
+
+
+class KitchenSinkDialogs(Screen):
     app = ObjectProperty()
-    input_dialog = None
+    simple_dialog = None
     alert_dialog = None
-    ok_cancel_dialog = None
-    long_dialog = None
+    custom_dialog = None
+    confirmation_dialog = None
 
-    def show_example_input_dialog(self):
-        """Creates an instance of the dialog box and displays it
-        on the screen for the screen Dialogs."""
-
-        def result(text_button, instance):
-            from kivymd.toast import toast
-
-            toast(instance.text_field.text)
-
-        if not self.input_dialog:
-            from kivymd.uix.dialog import MDInputDialog
-
-            self.input_dialog = MDInputDialog(
-                title="Title",
-                hint_text="Hint text",
-                size_hint=(0.8, 0.4),
-                text_button_ok="Ok",
-                events_callback=result,
+    def show_example_confirmation_dialog(self):
+        if not self.confirmation_dialog:
+            self.confirmation_dialog = MDDialog(
+                title="Phone ringtone",
+                type="confirmation",
+                items=[
+                    KitchenSinkItemConfirm(text="Callisto"),
+                    KitchenSinkItemConfirm(text="Luna"),
+                    KitchenSinkItemConfirm(text="Night"),
+                    KitchenSinkItemConfirm(text="Solo"),
+                    KitchenSinkItemConfirm(text="Phobos"),
+                    KitchenSinkItemConfirm(text="Diamond"),
+                    KitchenSinkItemConfirm(text="Sirena"),
+                    KitchenSinkItemConfirm(text="Red music"),
+                    KitchenSinkItemConfirm(text="Allergio"),
+                    KitchenSinkItemConfirm(text="Magic"),
+                    KitchenSinkItemConfirm(text="Tic-tac"),
+                ],
+                buttons=[
+                    MDFlatButton(
+                        text="CANCEL",
+                        text_color=self.app.theme_cls.primary_color,
+                    ),
+                    MDFlatButton(
+                        text="OK", text_color=self.app.theme_cls.primary_color
+                    ),
+                ],
             )
-        self.input_dialog.open()
+        self.confirmation_dialog.open()
+
+    def show_example_simple_dialog(self):
+        if not self.simple_dialog:
+            self.simple_dialog = MDDialog(
+                title="Set backup account",
+                type="simple",
+                items=[
+                    KitchenSinkOneLineLeftAvatarItem(
+                        text="user01@gmail.com", source=f"{os.environ['KITCHEN_SINK_ASSETS']}heattheatr.png"
+                    ),
+                    KitchenSinkOneLineLeftAvatarItem(
+                        text="user02@gmail.com", source=f"{os.environ['KITCHEN_SINK_ASSETS']}mountaino.png"
+                    ),
+                    KitchenSinkOneLineLeftAvatarItem(
+                        text="Add account", source=f"{os.environ['KITCHEN_SINK_ASSETS']}twitter-round.png"
+                    ),
+                ],
+            )
+        self.simple_dialog.open()
+
+    def show_example_custom_dialog(self):
+        if not self.custom_dialog:
+            self.custom_dialog = MDDialog(
+                title="Address:",
+                type="custom",
+                content_cls=KitchenSinkDialogsCustomContent(),
+                buttons=[
+                    MDFlatButton(
+                        text="CANCEL",
+                        text_color=self.app.theme_cls.primary_color,
+                    ),
+                    MDFlatButton(
+                        text="OK", text_color=self.app.theme_cls.primary_color
+                    ),
+                ],
+            )
+        self.custom_dialog.open()
 
     def show_example_alert_dialog(self):
         if not self.alert_dialog:
-            from kivymd.uix.dialog import MDDialog
-
             self.alert_dialog = MDDialog(
-                title="Title",
-                size_hint=(0.8, 0.4),
-                text_button_ok="Ok",
-                text="This is Alert dialog",
-                events_callback=self.app.callback_for_menu_items,
+                title="Reset settings?",
+                text="This will reset your device to its default factory settings.",
+                buttons=[
+                    MDFlatButton(
+                        text="CANCEL",
+                        text_color=self.app.theme_cls.primary_color,
+                    ),
+                    MDFlatButton(
+                        text="ACCEPT",
+                        text_color=self.app.theme_cls.primary_color,
+                    ),
+                ],
             )
         self.alert_dialog.open()
-
-    def show_example_ok_cancel_dialog(self):
-        if not self.ok_cancel_dialog:
-            from kivymd.uix.dialog import MDDialog
-
-            self.ok_cancel_dialog = MDDialog(
-                title="Title",
-                size_hint=(0.8, 0.4),
-                text_button_ok="Ok",
-                text="This is Ok Cancel dialog",
-                text_button_cancel="Cancel",
-                events_callback=self.app.callback_for_menu_items,
-            )
-        self.ok_cancel_dialog.open()
-
-    def show_example_long_dialog(self):
-        if not self.long_dialog:
-            from kivymd.uix.dialog import MDDialog
-
-            self.long_dialog = MDDialog(
-                text="Lorem ipsum dolor sit amet, consectetur adipiscing "
-                "elit, sed do eiusmod tempor incididunt ut labore et "
-                "dolore magna aliqua. Ut enim ad minim veniam, quis "
-                "nostrud exercitation ullamco laboris nisi ut aliquip "
-                "ex ea commodo consequat. Duis aute irure dolor in "
-                "reprehenderit in voluptate velit esse cillum dolore eu "
-                "fugiat nulla pariatur. Excepteur sint occaecat "
-                "cupidatat non proident, sunt in culpa qui officia "
-                "deserunt mollit anim id est laborum.",
-                title="Title",
-                size_hint=(0.8, 0.4),
-                text_button_ok="Yes",
-                events_callback=self.app.callback_for_menu_items,
-            )
-        self.long_dialog.open()

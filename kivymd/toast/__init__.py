e0e750e9bec6a88581f2a0786dc45958bb3b648f
macbookair <kivydevelopment@gmail.com>
2020-04-09 15:32:17 +0300

    Added import of the `kivytoast` module if the application runs on an `Android` device, but from the source codes

--- kivymd/toast/__init__.py
+++ kivymd/toast/__init__.py
@@ -2,6 +2,9 @@ from kivy.utils import platform
 
 
 if platform == "android":
-    from .androidtoast import toast
+    try:
+        from .androidtoast import toast
+    except:
+        from .kivytoast import toast
 else:
     from .kivytoast import toast

5509a679b49c848af94bf7a7dc13546f580ebbc1
sanderland <48946947+sanderland@users.noreply.github.com>
2020-05-29 23:40:03 +0200

    Update navigationdrawer.py

--- kivymd/uix/navigationdrawer.py
+++ kivymd/uix/navigationdrawer.py
@@ -624,7 +624,6 @@ class MDNavigationDrawer(MDCard):
         elif self.status == "opened":
             if (
                 self.close_on_click
-                and self.get_dist_from_side(touch.ox) > self.width
                 and not self.collide_point(touch.ox, touch.oy)
             ):
                 self.set_state("close", animation=True)

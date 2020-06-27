308e9856fe383dddfd7535f9756f50cdd4353dbc
macbookair <kivydevelopment@gmail.com>
2020-06-27 12:46:29 +0300

    Added setting the `velocity` attribute in the `on_value` method

--- kivymd/stiffscroll/__init__.py
+++ kivymd/stiffscroll/__init__.py
@@ -156,9 +156,11 @@ class StiffScrollEffect(KineticEffect):
     def on_value(self, *args):
         """Prevent moving beyond my bounds, and update ``self.scroll``"""
 
-        if self.value > 0:
+        if self.value > self.min:
+            self.velocity = 0
             self.scroll = self.min
         elif self.value < self.max:
+            self.velocity = 0
             self.scroll = self.max
         else:
             self.scroll = self.value

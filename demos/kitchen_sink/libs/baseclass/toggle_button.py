b445e523065796ba89f8c376eb6ddad76ee76df0
macbookair <kivydevelopment@gmail.com>
2020-07-10 01:02:05 +0300

    Fox `KitchenSinkToggleButtons` example

--- demos/kitchen_sink/libs/baseclass/toggle_button.py
+++ demos/kitchen_sink/libs/baseclass/toggle_button.py
@@ -2,30 +2,14 @@ from kivy.uix.screenmanager import Screen
 
 from kivymd.app import MDApp
 from kivymd.uix.behaviors.toggle_behavior import MDToggleButton
-from kivymd.uix.button import (
-    MDFillRoundFlatButton,
-    MDFillRoundFlatIconButton,
-    MDRectangleFlatButton,
-)
+from kivymd.uix.button import MDFillRoundFlatButton
 
 
 class KitchenSinkToggleButtons(Screen):
     pass
 
 
-class FillRoundFlatToggleButton(MDFillRoundFlatButton, MDToggleButton):
-    def __init__(self, **kwargs):
-        self.background_down = MDApp.get_running_app().theme_cls.primary_dark
-        super().__init__(**kwargs)
-
-
-class RectangleFlatButtonToggleButton(MDRectangleFlatButton, MDToggleButton):
-    def __init__(self, **kwargs):
-        self.background_down = MDApp.get_running_app().theme_cls.primary_light
-        super().__init__(**kwargs)
-
-
-class RoundFlatIconToggleButton(MDFillRoundFlatIconButton, MDToggleButton):
+class RoundFlatToggleButton(MDFillRoundFlatButton, MDToggleButton):
     def __init__(self, **kwargs):
         self.background_down = MDApp.get_running_app().theme_cls.accent_color
         super().__init__(**kwargs)

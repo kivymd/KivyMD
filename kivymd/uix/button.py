560699ab8cc226aebe7fc9260006a3d96e5df876
macbookair <kivydevelopment@gmail.com>
2020-08-13 10:27:11 +0300

    Edit doc string

--- kivymd/uix/button.py
+++ kivymd/uix/button.py
@@ -38,7 +38,34 @@ MDIconButton
 
 .. code-block:: python
 
-MDRectangleFlatIconButton
+.. MDIconButton:
+MDIconButton
+------------
+
+.. image:: https://github.com/HeaTTheatR/KivyMD-data/raw/master/gallery/kivymddoc/md-icon-button.gif
+    :align: center
+
+.. code-block:: python
+
+    from kivy.lang import Builder
+
+    from kivymd.app import MDApp
+
+    KV = '''
+    Screen:
+
+        MDIconButton:
+            icon: "language-python"
+            pos_hint: {"center_x": .5, "center_y": .5}
+    '''
+
+
+    class Example(MDApp):
+        def build(self):
+            return Builder.load_string(KV)
+
+
+    Example().run()
 
 The :class:`~MDIconButton.icon` parameter must have the name of the icon
 from ``kivymd/icon_definitions.py`` file.

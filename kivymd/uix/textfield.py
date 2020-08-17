65024b0d4cb7cebb2304e47ad788679704b081d6
macbookair <kivydevelopment@gmail.com>
2020-08-17 11:50:00 +0300

    Added `Clickable icon for MDTextFieldRound` section to doc string

--- kivymd/uix/textfield.py
+++ kivymd/uix/textfield.py
@@ -297,6 +297,69 @@ Control background color
 .. image:: https://github.com/HeaTTheatR/KivyMD-data/raw/master/gallery/kivymddoc/text-field-round-active-color.gif
     :align: center
 
+Clickable icon for MDTextFieldRound
+-----------------------------------
+
+.. code-block:: python
+
+    from kivy.lang import Builder
+    from kivy.properties import StringProperty
+
+    from kivymd.app import MDApp
+    from kivymd.uix.relativelayout import MDRelativeLayout
+
+    KV = '''
+    <ClickableTextFieldRound>:
+        size_hint_y: None
+        height: text_field.height
+
+        MDTextFieldRound:
+            id: text_field
+            hint_text: root.hint_text
+            text: root.text
+            password: True
+            color_active: app.theme_cls.primary_light
+            icon_left: "key-variant"
+            padding:
+                self._lbl_icon_left.texture_size[1] + dp(10) if self.icon_left else dp(15), \
+                (self.height / 2) - (self.line_height / 2), \
+                self._lbl_icon_right.texture_size[1] + dp(20), \
+                0
+
+        MDIconButton:
+            icon: "eye-off"
+            ripple_scale: .5
+            pos_hint: {"center_y": .5}
+            pos: text_field.width - self.width + dp(8), 0
+            on_release:
+                self.icon = "eye" if self.icon == "eye-off" else "eye-off"
+                text_field.password = False if text_field.password is True else True
+
+
+    MDScreen:
+
+        ClickableTextFieldRound:
+            size_hint_x: None
+            width: "300dp"
+            hint_text: "Password"
+            pos_hint: {"center_x": .5, "center_y": .5}
+    '''
+
+
+    class ClickableTextFieldRound(MDRelativeLayout):
+        text = StringProperty()
+        hint_text = StringProperty()
+        # Here specify the required parameters for MDTextFieldRound:
+        # [...]
+
+
+    class Test(MDApp):
+        def build(self):
+            return Builder.load_string(KV)
+
+
+    Test().run()
+
 With right icon
 ---------------
 

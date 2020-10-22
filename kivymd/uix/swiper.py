d5a4b629778f8b817355debcc861b3107bbd8b1e
macbookair <kivydevelopment@gmail.com>
2020-10-22 12:02:08 +0300

    Added `on_scroll_start` method for touch pad events. Edit doc strings

--- kivymd/uix/swiper.py
+++ kivymd/uix/swiper.py
@@ -97,6 +97,82 @@ Example
         on_swipe_left: print("on_swipe_left")
         on_swipe_right: print("on_swipe_right")
 
+Example
+=======
+
+.. code-block:: python
+
+    from kivy.lang.builder import Builder
+
+    from kivymd.app import MDApp
+
+    kv = '''
+    <MagicButton@MagicBehavior+MDIconButton>
+
+
+    <MySwiper@MDSwiperItem>
+
+        RelativeLayout:
+
+            FitImage:
+                source: "guitar.png"
+                radius: [20,]
+
+            MDBoxLayout:
+                adaptive_height: True
+                spacing: "12dp"
+
+                MagicButton:
+                    id: icon
+                    icon: "weather-sunny"
+                    user_font_size: "56sp"
+                    opposite_colors: True
+
+                MDLabel:
+                    text: "MDLabel"
+                    font_style: "H5"
+                    size_hint_y: None
+                    height: self.texture_size[1]
+                    pos_hint: {"center_y": .5}
+                    opposite_colors: True
+
+
+    MDScreen:
+
+        MDToolbar:
+            id: toolbar
+            title: "MDSwiper"
+            elevation: 10
+            pos_hint: {"top": 1}
+
+        MDSwiper:
+            size_hint_y: None
+            height: root.height - toolbar.height - dp(40)
+            y: root.height - self.height - toolbar.height - dp(20)
+            on_swipe: self.get_current_item().ids.icon.shake()
+
+            MySwiper:
+
+            MySwiper:
+
+            MySwiper:
+
+            MySwiper:
+
+            MySwiper:
+    '''
+
+
+    class Main(MDApp):
+        def build(self):
+            return Builder.load_string(kv)
+
+
+    Main().run()
+
+.. image:: https://github.com/HeaTTheatR/KivyMD-data/raw/master/gallery/kivymddoc/mdswiper-on-swipe.gif
+    :align: center
+
 How to automatically switch a SwiperItem?
 =========================================
 
@@ -428,6 +504,14 @@ class MDSwiper(ScrollView, EventDispatcher):
         else:
             self.set_current(next_index)
             self.dispatch("on_swipe_right")
+
+    def on_scroll_start(self, touch, check_children=True):
+        # on touch pad events
+        if touch.button == "scrollright":
+            self.swipe_left()
+        elif touch.button == "scrollleft":
+            self.swipe_right()
+        return super().on_scroll_start(touch)
 
     def on_touch_down(self, touch):
         super().on_touch_down(touch)

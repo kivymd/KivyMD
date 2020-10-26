8b6f0b00a2b4f0b2ba7a8814b72ff9d2bb540fff
macbookair <kivydevelopment@gmail.com>
2020-10-26 09:50:58 +0300

    `pre-commit run --all-files`

--- kivymd/uix/navigationrail.py
+++ kivymd/uix/navigationrail.py
@@ -753,10 +753,7 @@ class MDNavigationRail(MDCard):
                 )
             if self.floating_action_button:
                 Animation(
-                    _canvas_width=0,
-                    _padding_right=0,
-                    d=0.2,
-                    _alpha=0,
+                    _canvas_width=0, _padding_right=0, d=0.2, _alpha=0,
                 ).start(self.floating_action_button)
             self.dispatch("on_close")
 
@@ -813,9 +810,7 @@ class MDNavigationRail(MDCard):
     def on_use_action_button(self, instance, value):
         if value:
             rail_box = BaseNavigationRailBoxItem(
-                size_hint=(None, None),
-                width=self.width,
-                padding=dp(8),
+                size_hint=(None, None), width=self.width, padding=dp(8),
             )
             self.floating_action_button = BaseNavigationRailFloatingButton(
                 pos_hint={"top": 1},

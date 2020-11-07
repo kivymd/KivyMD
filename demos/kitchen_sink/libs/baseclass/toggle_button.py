9a14cea2e77703896c341913b750ce75a39a2d42
Kulothungan16 <kulothunganug@gmail.com>
2020-11-07 10:21:02 +0530

    added more togglebutton examples

--- demos/kitchen_sink/libs/baseclass/toggle_button.py
+++ demos/kitchen_sink/libs/baseclass/toggle_button.py
@@ -2,7 +2,7 @@ from kivy.uix.screenmanager import Screen
 
 from kivymd.app import MDApp
 from kivymd.uix.behaviors.toggle_behavior import MDToggleButton
-from kivymd.uix.button import MDFillRoundFlatButton
+from kivymd.uix.button import MDFillRoundFlatButton, MDRectangleFlatButton
 
 
 class KitchenSinkToggleButtons(Screen):
@@ -12,4 +12,10 @@ class KitchenSinkToggleButtons(Screen):
 class RoundFlatToggleButton(MDFillRoundFlatButton, MDToggleButton):
     def __init__(self, **kwargs):
         self.background_down = MDApp.get_running_app().theme_cls.accent_color
+        super().__init__(**kwargs)
+
+
+class FlatToggleButton(MDRectangleFlatButton, MDToggleButton):
+    def __init__(self, **kwargs):
+        self.background_down = MDApp.get_running_app().theme_cls.primary_light
         super().__init__(**kwargs)

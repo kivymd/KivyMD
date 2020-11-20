be3cc03810f659229ed44aed5fc10e136dc1ac5c
macbookair <kivydevelopment@gmail.com>
2020-11-20 22:21:00 +0300

    Added setting of parameters for the `MDLabel` class

--- kivymd/uix/__init__.py
+++ kivymd/uix/__init__.py
@@ -1,5 +1,6 @@
 from kivy.properties import BooleanProperty
 from kivy.uix.floatlayout import FloatLayout
+from kivy.uix.label import Label
 from kivy.uix.screenmanager import Screen
 
 from kivymd.uix.behaviors import SpecificBackgroundColorBehavior
@@ -46,16 +47,38 @@ class MDAdaptiveWidget(SpecificBackgroundColorBehavior):
     """
 
     def on_adaptive_height(self, instance, value):
-        if not isinstance(self, (FloatLayout, Screen)):
-            self.size_hint_y = None
-            self.bind(minimum_height=self.setter("height"))
+        self.size_hint_y = None
+        if issubclass(self.__class__, Label):
+            self.bind(
+                texture_size=lambda *x: self.setter("height")(
+                    self, self.texture_size[1]
+                )
+            )
+        else:
+            if not isinstance(self, (FloatLayout, Screen)):
+                self.bind(minimum_height=self.setter("height"))
 
     def on_adaptive_width(self, instance, value):
-        if not isinstance(self, (FloatLayout, Screen)):
-            self.size_hint_x = None
-            self.bind(minimum_width=self.setter("width"))
+        self.size_hint_x = None
+        if issubclass(self.__class__, Label):
+            self.bind(
+                texture_size=lambda *x: self.setter("width")(
+                    self, self.texture_size[0]
+                )
+            )
+        else:
+            if not isinstance(self, (FloatLayout, Screen)):
+                self.bind(minimum_width=self.setter("width"))
 
     def on_adaptive_size(self, instance, value):
-        if not isinstance(self, (FloatLayout, Screen)):
-            self.size_hint = (None, None)
-            self.bind(minimum_size=self.setter("size"))
+        self.size_hint = (None, None)
+        if issubclass(self.__class__, Label):
+            self.text_size = (None, None)
+            self.bind(
+                texture_size=lambda *x: self.setter("size")(
+                    self, self.texture_size
+                )
+            )
+        else:
+            if not isinstance(self, (FloatLayout, Screen)):
+                self.bind(minimum_size=self.setter("size"))

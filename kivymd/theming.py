3578d42f803992b6f4a5695998b502d56c14af55
Kulothungan16 <kulothunganug@gmail.com>
2020-12-21 00:17:46 +0530

    decrease line (#725)

--- kivymd/theming.py
+++ kivymd/theming.py
@@ -782,12 +782,9 @@ class ThemeManager(EventDispatcher):
     def set_clearcolor_by_theme_style(self, theme_style):
         if not self.set_clearcolor:
             return
-        if theme_style == "Light":
-            Window.clearcolor = get_color_from_hex(
-                colors["Light"]["Background"]
-            )
-        elif theme_style == "Dark":
-            Window.clearcolor = get_color_from_hex(colors["Dark"]["Background"])
+        Window.clearcolor = get_color_from_hex(
+            colors[theme_style]["Background"]
+        )
 
     # font name, size (sp), always caps, letter spacing (sp)
     font_styles = DictProperty(

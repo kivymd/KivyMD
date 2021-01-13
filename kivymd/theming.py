5e880ae5585987e8083732a753e5461040c02486
runette <runette@gmail.com>
2021-01-13 15:30:39 +0000

    Added documentation

--- kivymd/theming.py
+++ kivymd/theming.py
@@ -19,6 +19,42 @@ Control material properties
 
 The main application class inherited from the `MDApp` class has the :attr:`theme_cls`
 attribute, with which you control the material properties of your application.
+
+Changing the theme colors
+-------------------------
+
+The standard theme_cls is designed to provide the standard themes and colors as
+defined by Material Design.
+
+We do not recommend that you change this.
+
+However, if you do need to change the standard colors, for instance to meet branding
+guidelines, you can do this by overloading the `color_definitions.py` object.
+
+* Create a custom copy of `color_definitions.py`,
+* Change the color codes in `colors` object in your custum `color_definitions.py`
+file as required. Note that you *cannot* change the names of the colors, you can only
+change the values. That is say, for instance, you cannot change the name of `Blue` and
+you cannot add `NavyBlue` but you change the value of the color blue to meet your 
+requirements.
+* Import the custom colors into your module and do something like this (where the custom 
+version of `color_defintions.py` is called `custom_palette.py`):
+
+.. code-block:: python
+
+    from custom_palette import colors
+
+    class Example(MDApp):
+        icons = list(md_icons.keys())[15:30]
+
+        def __init__(self, **kwargs):
+            super(Example, self).__init__(**kwargs)
+            self.theme_cls.colors = colors
+
+This will change the theme colors to your custom defintion. In all other respects,
+the theming stays as documented.
+
+
 """
 
 from kivy.app import App

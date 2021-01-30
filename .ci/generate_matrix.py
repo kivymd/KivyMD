fb3fc1fcca9a7b3ec1cb326438969157dd77e1c1
Artem Bulgakov <ArtemSBulgakov@ya.ru>
2021-01-30 22:44:10 +0300

    Add script generate_matrix.py
    Build only demos that need to be rebuilt

---
+++ .ci/generate_matrix.py
@@ -0,0 +1,34 @@
+#!/bin/python3
+# Generates matrix for GitHub Actions
+# For workflow https://github.com/kivymd/KivyMD/blob/master/.github/workflows/build-demos.yml
+# See https://stackoverflow.com/a/62953566/11948346
+
+import json
+import os
+import sys
+from pathlib import Path
+
+diff = sys.argv[1]
+all_demos = ["kitchen_sink", "crane", "fortnightly", "rally", "shrine"]
+demos_android = []
+
+for filename in diff.split("\n"):
+    filename = Path(filename)
+    if str(filename).split(os.sep, 1)[0] in ("kivymd",) or filename in (
+        ".github/workflows/build-demos.yml",
+        "setup.py",
+        "setup.cfg",
+        ".ci/move_binary",
+    ):
+        demos_android = all_demos
+        break
+    if str(filename).startswith("demos"):
+        demo_name = str(filename).split(os.sep, 2)[1]
+        demos_android.extend([demo_name])
+
+matrix_android = {"include": []}
+for demo_name in demos_android:
+    matrix_android["include"].append({"demo-name": demo_name})
+
+print("matrix-android:", json.dumps(matrix_android))
+print("::set-output name=matrix-android::" + json.dumps(matrix_android))

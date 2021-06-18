b566eb2d033bfa1e4f1d81fbde21e70e65f4d79c
macbookair <kivydevelopment@gmail.com>
2021-06-18 20:59:02 +0300

    Remove `MDStepper` from `Factory.redister`

--- kivymd/factory_registers.py
+++ kivymd/factory_registers.py
@@ -5,7 +5,7 @@ Register KivyMD widgets to use without import
 from kivy.factory import Factory
 
 r = Factory.register
-r("MDStepper", module="kivymd.uix.stepper")
+# r("MDStepper", module="kivymd.uix.stepper")
 r("MDNavigationRail", module="kivymd.uix.navigationrail")
 r("MDSwiper", module="kivymd.uix.swiper")
 r("MDCarousel", module="kivymd.uix.carousel")

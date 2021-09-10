1d61604eb8ea0d2619245de1be8dad1ef654a24f
macbookair <kivydevelopment@gmail.com>
2021-09-10 20:32:51 +0300

    Adding files of type KV to the package
    Fixed missing KV files when building a package using the `PyInstaller`
    tool.

--- kivymd/tools/packaging/pyinstaller/hook-kivymd.py
+++ kivymd/tools/packaging/pyinstaller/hook-kivymd.py
@@ -2,22 +2,41 @@
 PyInstaller hook for KivyMD
 ===========================
 
-Adds fonts and images to package.
+Adds fonts, images and KV files to package.
 
 All modules from uix directory are added by Kivy hook.
 """
 
+import os
 from pathlib import Path
 
 import kivymd
 
 datas = [
+    # Add `.ttf` files from the `kivymd/fonts` directory.
     (
         kivymd.fonts_path,
         str(Path("kivymd").joinpath(Path(kivymd.fonts_path).name)),
     ),
+    # Add files from the `kivymd/images` directory.
     (
         kivymd.images_path,
         str(Path("kivymd").joinpath(Path(kivymd.images_path).name)),
     ),
 ]
+
+# Add `.kv. files from the `kivymd/uix` directory.
+for path_to_kv_file in Path(kivymd.uix_path).glob("**/*.kv"):
+    datas.append(
+        (
+            str(Path(path_to_kv_file).parent.joinpath("*.kv")),
+            str(
+                Path("kivymd").joinpath(
+                    "uix",
+                    str(Path(path_to_kv_file).parent).split(
+                        str(Path("kivymd").joinpath("uix")) + os.sep
+                    )[1],
+                )
+            ),
+        )
+    )

26b5ae1cef7a1af866df14314441de76a0d8828d
macbookair <kivydevelopment@gmail.com>
2021-09-27 22:22:33 +0300

    Fix doc string

--- kivymd/uix/tab/tab.py
+++ kivymd/uix/tab/tab.py
@@ -429,26 +429,24 @@ Switching the tab by name
 
 
     <Tab>
+
         MDBoxLayout:
             orientation: "vertical"
             pos_hint: {"center_x": .5, "center_y": .5}
             size_hint: None, None
             spacing: dp(48)
+
             MDIconButton:
                 id: icon
                 icon: "arrow-right"
                 user_font_size: "48sp"
-
-                on_release:
-                    app.switch_tab_by_name()
+                on_release: app.switch_tab_by_name()
 
             MDIconButton:
                 id: icon2
                 icon: "page-next"
                 user_font_size: "48sp"
-
-                on_release:
-                    app.switch_tab_by_object()
+                on_release: app.switch_tab_by_object()
     '''
 
 
@@ -461,8 +459,7 @@ Switching the tab by name
 
         def build(self):
             self.iter_list_names = iter(list(self.icons))
-            root = Builder.load_string(KV)
-            return root
+            return Builder.load_string(KV)
 
         def on_start(self):
             for name_tab in list(self.icons):
@@ -472,26 +469,24 @@ Switching the tab by name
         def switch_tab_by_object(self):
             try:
                 x = next(self.iter_list_objects)
-                print(f"Switch slide by object, \n\tnex element to show: [{x}]")
+                print(f"Switch slide by object, next element to show: [{x}]")
                 self.root.ids.tabs.switch_tab(x)
             except StopIteration:
                 # reset the iterator an begin again.
                 self.iter_list_objects = iter(list(self.root.ids.tabs.get_tab_list()))
                 self.switch_tab_by_object()
-                pass
 
         def switch_tab_by_name(self):
             '''Switching the tab by name.'''
 
             try:
                 x = next(self.iter_list_names)
-                print(f"Switch slide by name, \n\tnex element to show: [{x}]")
+                print(f"Switch slide by name, next element to show: [{x}]")
                 self.root.ids.tabs.switch_tab(x)
             except StopIteration:
-                # reset the iterator an begin again.
+                # Reset the iterator an begin again.
                 self.iter_list_names = iter(list(self.icons))
                 self.switch_tab_by_name()
-                pass
 
 
     Example().run()

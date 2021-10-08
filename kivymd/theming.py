9fc99af7c5ecfb392a255e0e3f9a26d36e0b6163
mkr <47008271+max-mkr@users.noreply.github.com>
2021-10-08 12:28:24 +0300

    Corrected import for screen (#1095)
    Existing import statement results in `ModuleNotFoundError: No module named 'kivymd.screen'`

--- kivymd/theming.py
+++ kivymd/theming.py
@@ -479,7 +479,7 @@ class ThemeManager(EventDispatcher):
     .. code-block:: python
 
         from kivymd.app import MDApp
-        from kivymd.screen import MDScreen
+        from kivymd.uix.screen import MDScreen
         from kivymd.uix.button import MDRectangleFlatButton
 
 

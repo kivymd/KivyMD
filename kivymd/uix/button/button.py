3f18c25b31160503eb767a1e5ea79fe250686403
Andrew McLeod <andrewclimbing@gmail.com>
2021-12-09 11:48:42 +0000

    fix(buttons): Fix issue where python-created disabled buttons had extra shadows

--- kivymd/uix/button/button.py
+++ kivymd/uix/button/button.py
@@ -944,6 +944,7 @@ class ButtonElevationBehaviour(CommonElevationBehavior):
         super().__init__(**kwargs)
         self.bind(_radius=self.setter('radius'))
         self.on_elevation(self, self.elevation)
+        self.on_disabled(self, self.disabled)
 
     def on_elevation(self, instance_button, elevation_value: int) -> NoReturn:
         super().on_elevation(instance_button, elevation_value)

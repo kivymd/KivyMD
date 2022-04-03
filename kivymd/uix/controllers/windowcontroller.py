dcf3520f6d75324dc604756ae383fc841b8bb1c3
macbookair <kivydevelopment@gmail.com>
2022-04-03 23:35:15 +0300

    Add doc string

--- kivymd/uix/controllers/windowcontroller.py
+++ kivymd/uix/controllers/windowcontroller.py
@@ -1,6 +1,31 @@
 """
 Controllers/WindowController
 ============================
+
+Controlling the resizing direction of the application window
+------------------------------------------------------------
+
+.. code-block:: python
+
+    # When resizing the application window, the direction of change will be
+    # printed - 'left' or 'right'.
+
+    from kivymd.app import MDApp
+    from kivymd.uix.controllers import WindowController
+    from kivymd.uix.screen import MDScreen
+
+
+    class MyScreen(MDScreen, WindowController):
+        def on_width(self, *args):
+            print(self.get_window_width_resizing_direction())
+
+
+    class Test(MDApp):
+        def build(self):
+            return MyScreen()
+
+
+    Test().run()
 """
 
 from kivy.core.window import Window

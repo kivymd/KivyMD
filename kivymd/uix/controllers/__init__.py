5dff415f411df3e8201ee4dd1e750a8d9816ef58
macbookair <kivydevelopment@gmail.com>
2022-04-03 23:19:24 +0300

    Add import `WindowController` class

--- kivymd/uix/controllers/__init__.py
+++ kivymd/uix/controllers/__init__.py
@@ -7,3 +7,5 @@ Controllers
 Modules and classes that implement useful methods for getting information
 about the state of the current application window.
 """
+
+from .windowcontroller import WindowController

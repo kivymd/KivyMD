9ed3d1d8af78ce5d402e77763cd09728945eaf93
macbookair <kivydevelopment@gmail.com>
2022-04-03 14:04:33 +0300

    Add `controller` package
    Modules and classes that implement useful methods for getting information
    about the state of the current application window.

---
+++ kivymd/uix/controller/__init__.py
@@ -0,0 +1,32 @@
+"""
+Controller
+==========
+
+.. versionadded:: 1.0.0
+
+Modules and classes that implement useful methods for getting information
+about the state of the current application window.
+"""
+
+from kivy.core.window import Window
+from kivy.core.window.window_sdl2 import WindowSDL
+
+
+class WindowController:
+    def __init__(self):
+        self.window_resizing_direction = "unknown"
+        self.__width = Window.width
+        Window.bind(on_resize=self._on_resize)
+
+    def get_window_width_resizing_direction(self) -> str:
+        return self.window_resizing_direction
+
+    def _set_window_width_resizing_direction(self, width: int) -> None:
+        if self.__width > width:
+            self.window_resizing_direction = "left"
+        elif self.__width < width:
+            self.window_resizing_direction = "right"
+
+    def _on_resize(self, window_sdl2: WindowSDL, width: int, height: int) -> None:
+        self._set_window_width_resizing_direction(width)
+        self.__width = width

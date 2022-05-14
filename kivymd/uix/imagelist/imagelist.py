a944413143f5f31795e88309413ad474fc16f2ba
macbookair <kivydevelopment@gmail.com>
2022-05-14 09:01:34 +0300

    Refactoring the `SmartTile` widget
    - removed unnecessary widgets;
    - updated documentation;
    - changed `API`;

--- kivymd/uix/imagelist/imagelist.py
+++ kivymd/uix/imagelist/imagelist.py
@@ -13,60 +13,8 @@ Components/ImageList
 
 `KivyMD` provides the following tile classes for use:
 
-- SmartTileWithStar_
-- SmartTileWithLabel_
-
-.. SmartTileWithStar:
-SmartTileWithStar
------------------
-
-.. code-block::
-
-    from kivy.lang import Builder
-
-    from kivymd.app import MDApp
-
-    KV = '''
-    <MyTile@SmartTileWithStar>
-        size_hint_y: None
-        height: "240dp"
-
-
-    ScrollView:
-
-        MDGridLayout:
-            cols: 3
-            adaptive_height: True
-            padding: dp(4), dp(4)
-            spacing: dp(4)
-
-            MyTile:
-                stars: 5
-                source: "cat-1.jpg"
-
-            MyTile:
-                stars: 5
-                source: "cat-2.jpg"
-
-            MyTile:
-                stars: 5
-                source: "cat-3.jpg"
-    '''
-
-
-    class MyApp(MDApp):
-        def build(self):
-            return Builder.load_string(KV)
-
-
-    MyApp().run()
-
-.. image:: https://github.com/HeaTTheatR/KivyMD-data/raw/master/gallery/kivymddoc/SmartTileWithStar.gif
-    :align: center
-
-.. SmartTileWithLabel:
-SmartTileWithLabel
-------------------
+Usage
+-----
 
 .. code-block:: python
 
@@ -75,32 +23,28 @@ SmartTileWithLabel
     from kivymd.app import MDApp
 
     KV = '''
-    <MyTile@SmartTileWithLabel>
-        size_hint_y: None
-        height: "240dp"
+    MDScreen:
 
+        MDSmartTile:
+            radius: 24
+            box_radius: [0, 0, 24, 24]
+            box_color: 1, 1, 1, .2
+            source: "cats.jpg"
+            pos_hint: {"center_x": .5, "center_y": .5}
+            size_hint: None, None
+            size: "320dp", "320dp"
 
-    ScrollView:
+            MDIconButton:
+                icon: "heart-outline"
+                theme_icon_color: "Custom"
+                icon_color: 1, 0, 0, 1
+                pos_hint: {"center_y": .5}
+                on_release: self.icon = "heart" if self.icon == "heart-outline" else "heart-outline"
 
-        MDGridLayout:
-            cols: 3
-            adaptive_height: True
-            padding: dp(4), dp(4)
-            spacing: dp(4)
-
-            MyTile:
-                source: "cat-1.jpg"
-                text: "[size=26]Cat 1[/size]\\n[size=14]cat-1.jpg[/size]"
-
-            MyTile:
-                source: "cat-2.jpg"
-                text: "[size=26]Cat 2[/size]\\n[size=14]cat-2.jpg[/size]"
-                tile_text_color: app.theme_cls.accent_color
-
-            MyTile:
-                source: "cat-3.jpg"
-                text: "[size=26][color=#ffffff]Cat 3[/color][/size]\\n[size=14]cat-3.jpg[/size]"
-                tile_text_color: app.theme_cls.accent_color
+            MDLabel:
+                text: "Julia and Julie"
+                bold: True
+                color: 1, 1, 1, 1
     '''
 
 
@@ -111,30 +55,38 @@ SmartTileWithLabel
 
     MyApp().run()
 
-.. image:: https://github.com/HeaTTheatR/KivyMD-data/raw/master/gallery/kivymddoc/SmartTileWithLabel.png
+.. image:: https://github.com/HeaTTheatR/KivyMD-data/raw/master/gallery/kivymddoc/md-smart-tile-usage.gif
+    :align: center
+
+Implementation
+--------------
+
+.. image:: https://github.com/HeaTTheatR/KivyMD-data/raw/master/gallery/kivymddoc/md-smart-tile-usage-sceleton.png
     :align: center
 """
 
-__all__ = ("SmartTile", "SmartTileWithLabel", "SmartTileWithStar")
+__all__ = ("MDSmartTile", "SmartTileWithLabel", "SmartTileWithStar")
 
 import os
 
+from kivy.logger import Logger
 from kivy.lang import Builder
 from kivy.properties import (
     BooleanProperty,
     ColorProperty,
-    NumericProperty,
-    ObjectProperty,
     OptionProperty,
     StringProperty,
+    VariableListProperty,
 )
 from kivy.uix.behaviors import ButtonBehavior
 
 from kivymd import uix_path
 from kivymd.theming import ThemableBehavior
 from kivymd.uix.behaviors import RectangularRippleBehavior
-from kivymd.uix.button import MDIconButton
-from kivymd.uix.floatlayout import MDFloatLayout
+from kivymd.uix.boxlayout import MDBoxLayout
+from kivymd.uix.fitimage import FitImage
+from kivymd.uix.label import MDLabel
+from kivymd.uix.relativelayout import MDRelativeLayout
 
 with open(
     os.path.join(uix_path, "imagelist", "imagelist.kv"), encoding="utf-8"
@@ -142,9 +94,15 @@ with open(
     Builder.load_string(kv_file.read())
 
 
-class SmartTile(
-    ThemableBehavior, RectangularRippleBehavior, ButtonBehavior, MDFloatLayout
-):
+class SmartTileImage(RectangularRippleBehavior, ButtonBehavior, FitImage):
+    """Implements the tile image."""
+
+
+class SmartTileOverlayBox(MDBoxLayout):
+    """Implements a container for custom widgets to be added to the tile."""
+
+
+class MDSmartTile(ThemableBehavior, MDRelativeLayout):
     """
     A tile for more complex needs.
 
@@ -152,9 +110,38 @@ class SmartTile(
     as a header or a footer, as described in the Material Design specs.
     """
 
+    box_radius = VariableListProperty([0], length=4)
+    """
+    Box radius.
+
+    .. versionadded:: 1.0.0
+
+    .. code-block:: kv
+    
+        MDSmartTile:
+            radius: 24
+            box_radius: [0, 0, 24, 24]
+
+    .. image:: https://github.com/HeaTTheatR/KivyMD-data/raw/master/gallery/kivymddoc/md-smart-tile-box-radius.png
+        :align: center
+
+    :attr:`box_radius` is an :class:`~kivy.properties.VariableListProperty`
+    and defaults to `[0, 0, 0, 0]`.
+    """
+
     box_color = ColorProperty((0, 0, 0, 0.5))
     """
     Sets the color and opacity for the information box.
+
+    .. code-block:: kv
+    
+        MDSmartTile:
+            radius: 24
+            box_radius: [0, 0, 24, 24]
+            box_color: 0, 1, 0, .5
+
+    .. image:: https://github.com/HeaTTheatR/KivyMD-data/raw/master/gallery/kivymddoc/md-smart-tile-box-color.png
+        :align: center
 
     :attr:`box_color` is a :class:`~kivy.properties.ColorProperty`
     and defaults to `(0, 0, 0, 0.5)`.
@@ -162,28 +149,77 @@ class SmartTile(
 
     box_position = OptionProperty("footer", options=["footer", "header"])
     """
-    Determines wether the information box acts as a header or footer to the
+    Determines weather the information box acts as a header or footer to the
     image. Available are options: `'footer'`, `'header'`.
+
+    .. code-block:: kv
+
+        MDSmartTile:
+            radius: 24
+            box_radius: [24, 24, 0, 0]
+            box_position: "header"
+
+    .. image:: https://github.com/HeaTTheatR/KivyMD-data/raw/master/gallery/kivymddoc/md-smart-tile-box-position.png
+        :align: center
 
     :attr:`box_position` is a :class:`~kivy.properties.OptionProperty`
     and defaults to `'footer'`.
-    """
-
-    lines = OptionProperty(1, options=[1, 2])
-    """
-    Number of lines in the `header/footer`. As per `Material Design specs`,
-    only 1 and 2 are valid values. Available are options: ``1``, ``2``.
-
-    :attr:`lines` is a :class:`~kivy.properties.OptionProperty`
-    and defaults to `1`.
     """
 
     overlap = BooleanProperty(True)
     """
     Determines if the `header/footer` overlaps on top of the image or not.
 
+    .. code-block:: kv
+
+        MDSmartTile:
+            radius: [24, 24, 0, 0]
+            box_radius: [0, 0, 24, 24]
+            overlap: False
+
+    .. image:: https://github.com/HeaTTheatR/KivyMD-data/raw/master/gallery/kivymddoc/md-smart-tile-overlap.png
+        :align: center
+
     :attr:`overlap` is a :class:`~kivy.properties.BooleanProperty`
     and defaults to `True`.
+    """
+
+    lines = OptionProperty(1, options=[1, 2])
+    """
+    Number of lines in the `header/footer`. As per `Material Design specs`,
+    only 1 and 2 are valid values. Available are options: `1`, `2`.
+    This parameter just increases the height of the container for custom
+    elements.
+
+    .. code-block:: kv
+
+        MDSmartTile:
+            radius: 24
+            box_radius: [0, 0, 24, 24]
+            lines: 2
+            source: "cats.jpg"
+            pos_hint: {"center_x": .5, "center_y": .5}
+            size_hint: None, None
+            size: "320dp", "320dp"
+
+            MDIconButton:
+                icon: "heart-outline"
+                theme_icon_color: "Custom"
+                icon_color: 1, 0, 0, 1
+                pos_hint: {"center_y": .5}
+                on_release: self.icon = "heart" if self.icon == "heart-outline" else "heart-outline"
+
+            TwoLineListItem:
+                text: "[color=#ffffff][b]My cats[/b][/color]"
+                secondary_text: "[color=#808080][b]Julia and Julie[/b][/color]"
+                pos_hint: {"center_y": .5}
+                _no_ripple_effect: True
+
+    .. image:: https://github.com/HeaTTheatR/KivyMD-data/raw/master/gallery/kivymddoc/md-smart-tile-lines.png
+        :align: center
+
+    :attr:`lines` is a :class:`~kivy.properties.OptionProperty`
+    and defaults to `1`.
     """
 
     source = StringProperty()
@@ -194,63 +230,44 @@ class SmartTile(
     and defaults to `''`.
     """
 
-    _img_widget = ObjectProperty()
-    _img_overlay = ObjectProperty()
-    _box_overlay = ObjectProperty()
-    _box_label = ObjectProperty()
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
 
-    def reload(self):
-        self._img_widget.reload()
+    def add_widget(self, widget, *args, **kwargs):
+        if isinstance(widget, (SmartTileImage, SmartTileOverlayBox)):
+            return super().add_widget(widget, *args, **kwargs)
+        else:
+            if isinstance(widget, MDLabel):
+                widget.shorten = True
+                widget.shorten_from = "right"
+            self.ids.box.add_widget(widget)
 
 
-class SmartTileWithLabel(SmartTile):
-    font_style = StringProperty("Caption")
+class SmartTileWithLabel(MDSmartTile):
     """
-    Tile font style.
-
-    :attr:`font_style` is a :class:`~kivy.properties.StringProperty`
-    and defaults to `'Caption'`.
-    """
-
-    tile_text_color = ColorProperty((1, 1, 1, 1))
-    """
-    Tile text color in ``rgba`` format.
-
-    :attr:`tile_text_color` is a :class:`~kivy.properties.ColorProperty`
-    and defaults to `(1, 1, 1, 1)`.
+    .. deprecated:: 1.0.0
+        Use :class:`~MDSmartTile` class instead.
     """
 
-    text = StringProperty()
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        Logger.warning(
+            "KivyMD: "
+            "The `SmartTileWithLabel` class has been deprecated. "
+            "Use the `MDSmartTile` class instead`"
+        )
+
+
+class SmartTileWithStar(MDSmartTile):
     """
-    Determines the text for the box `footer/header`.
-
-    :attr:`text` is a :class:`~kivy.properties.StringProperty`
-    and defaults to `''`.
-    """
-
-    _box_label = ObjectProperty()
-
-
-class SmartTileWithStar(SmartTileWithLabel):
-    stars = NumericProperty(1)
-    """
-    Tile stars.
-
-    :attr:`stars` is a :class:`~kivy.properties.NumericProperty`
-    and defaults to `1`.
+    .. deprecated:: 1.0.0
+        Use :class:`~MDSmartTile` class instead.
     """
 
-    def on_stars(self, *args) -> None:
-        for star in range(self.stars):
-            self.ids.box.add_widget(
-                _Star(
-                    icon="star-outline",
-                    theme_text_color="Custom",
-                    text_color=(1, 1, 1, 1),
-                )
-            )
-
-
-class _Star(MDIconButton):
-    def on_touch_down(self, touch):
-        return True
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        Logger.warning(
+            "KivyMD: "
+            "The `SmartTileWithStar` class has been deprecated. "
+            "Use the `MDSmartTile` class instead`"
+        )

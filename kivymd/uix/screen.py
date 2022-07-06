34b4b24a30a310e69774b586623f203a54cfa4cb
macbookair <kivydevelopment@gmail.com>
2022-07-06 13:16:02 +0300

    Adds the feature to use Hero animations.

--- kivymd/uix/screen.py
+++ kivymd/uix/screen.py
@@ -29,10 +29,32 @@ MDScreen
         md_bg_color: app.theme_cls.primary_color
 """
 
+from kivy.properties import ObjectProperty
 from kivy.uix.screenmanager import Screen
 
 from kivymd.uix import MDAdaptiveWidget
+from kivymd.uix.hero import MDHeroTo
 
 
 class MDScreen(Screen, MDAdaptiveWidget):
-    pass
+    hero_to = ObjectProperty()
+    """
+    Must be a  :class:`~kivymd.uix.hero.MDHeroTo` class.
+    See the documentation of the
+    `MDHeroTo <https://kivymd.readthedocs.io/en/latest/components/hero/>`_
+    widget for more detailed information.
+
+    .. versionchanged:: 1.0.0
+
+    :attr:`hero_to` is an :class:`~kivy.properties.ObjectProperty`
+    and defaults to `None`.
+    """
+
+    def on_hero_to(self, screen, widget) -> None:
+        if not isinstance(widget, MDHeroTo) or not issubclass(
+            widget.__class__, MDHeroTo
+        ):
+            raise TypeError(
+                f"The `{widget}` widget must be an `kivymd.uix.hero.MDHeroTo` "
+                f"class or inherited from this class"
+            )

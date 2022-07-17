4173cf62ed6ec7c7f43c1489a4a7d62602e3e56c
macbookair <kivydevelopment@gmail.com>
2022-07-17 13:37:44 +0300

    Edit doc string

--- kivymd/uix/controllers/windowcontroller.py
+++ kivymd/uix/controllers/windowcontroller.py
@@ -2,6 +2,8 @@
 Controllers/WindowController
 ============================
 
+.. versionadded:: 1.0.0
+
 Modules and classes that implement useful methods for getting information
 about the state of the current application window.
 

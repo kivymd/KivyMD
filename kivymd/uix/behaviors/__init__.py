bd91d3c94e53af2eb42db73aa8b1cb5eba48d9f5
macbookair <kivydevelopment@gmail.com>
2022-07-25 22:50:15 +0300

    Add import `DeclarativeBehavior` class

--- kivymd/uix/behaviors/__init__.py
+++ kivymd/uix/behaviors/__init__.py
@@ -5,12 +5,13 @@ Behaviors
 Modules and classes implementing various behaviors for buttons etc.
 """
 
-# flake8: NOQA
-from .hover_behavior import HoverBehavior  # isort:skip
 from .backgroundcolor_behavior import (
     BackgroundColorBehavior,
     SpecificBackgroundColorBehavior,
 )
+
+# flake8: NOQA
+from .declarative_bahavior import DeclarativeBehavior
 from .elevation import (
     CircularElevationBehavior,
     CommonElevationBehavior,
@@ -24,3 +25,5 @@ from .focus_behavior import FocusBehavior
 from .magic_behavior import MagicBehavior
 from .ripple_behavior import CircularRippleBehavior, RectangularRippleBehavior
 from .touch_behavior import TouchBehavior
+
+from .hover_behavior import HoverBehavior  # isort:skip

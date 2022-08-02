kivymd/KivyMD

commit 58f1faa3f452d7b57cccd97a1e7ab2f1b605b9c4

Fix doc string

kivymd/uix/expansionpanel/expansionpanel.py
@@ -56,6 +56,8 @@ Example
 
 .. code-block:: python
 
+    impoer os
+
     from kivy.lang import Builder
 
     from kivymd.app import MDApp
@@ -75,7 +77,7 @@ Example
                 icon: 'phone'
 
 
-    ScrollView:
+    MDScrollView:
 
         MDGridLayout:
             id: box
@@ -96,7 +98,7 @@ Example
             for i in range(10):
                 self.root.ids.box.add_widget(
                     MDExpansionPanel(
-                        icon=f"{images_path}kivymd.png",
+                        icon=os.path.join(images_path, "logo", "kivymd-icon-128.png"),
                         content=Content(),
                         panel_cls=MDExpansionPanelThreeLine(
                             text="Text",

commit 25a44343b34684f1fcfade6bdc37cbfc26893e7c

Fix typo, introduced in 58f1faa3f

kivymd/uix/expansionpanel/expansionpanel.py
@@ -56,7 +56,7 @@ Example
 
 .. code-block:: python
 
-    impoer os
+    import os
 
     from kivy.lang import Builder
 
4df275d525fc7bf0f804b4bf99cedcb5d25de661
macbookair <kivydevelopment@gmail.com>
2022-08-26 18:49:40 +0300

    Fix doc string

--- kivymd/uix/pickers/datepicker/datepicker.py
+++ kivymd/uix/pickers/datepicker/datepicker.py
@@ -17,54 +17,52 @@ Components/DatePicker
 
     .. tab:: Declarative KV style
 
-.. code-block:: python
+        .. code-block:: python
 
-        from kivy.lang import Builder
+            from kivy.lang import Builder
 
-        from kivymd.app import MDApp
-        from kivymd.uix.pickers import MDDatePicker
+            from kivymd.app import MDApp
+            from kivymd.uix.pickers import MDDatePicker
 
-        KV = '''
-        MDFloatLayout:
+            KV = '''
+            MDFloatLayout:
 
-            MDRaisedButton:
-                text: "Open date picker"
-                pos_hint: {'center_x': .5, 'center_y': .5}
-                on_release: app.show_date_picker()
-        '''
-
-
-        class Test(MDApp):
-            def build(self):
-                self.theme_cls.theme_style = "Dark"
-                self.theme_cls.primary_palette = "Orange"
-                return Builder.load_string(KV)
-
-            def on_save(self, instance, value, date_range):
-                '''
-                Events called when the "OK" dialog box button is clicked.
-
-                :type instance: <kivymd.uix.picker.MDDatePicker object>;
-
-                :param value: selected date;
-                :type value: <class 'datetime.date'>;
-
-                :param date_range: list of 'datetime.date' objects in the selected range;
-                :type date_range: <class 'list'>;
-                '''
-
-                print(instance, value, date_range)
-
-            def on_cancel(self, instance, value):
-                '''Events called when the "CANCEL" dialog box button is clicked.'''
-
-            def show_date_picker(self):
-                date_dialog = MDDatePicker()
-                date_dialog.bind(on_save=self.on_save, on_cancel=self.on_cancel)
-                date_dialog.open()
+                MDRaisedButton:
+                    text: "Open date picker"
+                    pos_hint: {'center_x': .5, 'center_y': .5}
+                    on_release: app.show_date_picker()
+            '''
 
 
-        Test().run()
+            class Test(MDApp):
+                def build(self):
+                    self.theme_cls.theme_style = "Dark"
+                    self.theme_cls.primary_palette = "Orange"
+                    return Builder.load_string(KV)
+
+                def on_save(self, instance, value, date_range):
+                    '''
+                    Events called when the "OK" dialog box button is clicked.
+
+                    :type instance: <kivymd.uix.picker.MDDatePicker object>;
+                    :param value: selected date;
+                    :type value: <class 'datetime.date'>;
+                    :param date_range: list of 'datetime.date' objects in the selected range;
+                    :type date_range: <class 'list'>;
+                    '''
+
+                    print(instance, value, date_range)
+
+                def on_cancel(self, instance, value):
+                    '''Events called when the "CANCEL" dialog box button is clicked.'''
+
+                def show_date_picker(self):
+                    date_dialog = MDDatePicker()
+                    date_dialog.bind(on_save=self.on_save, on_cancel=self.on_cancel)
+                    date_dialog.open()
+
+
+            Test().run()
 
     .. tab:: Declarative python style
 

afa35dc9053feea74a9dd3f18ca26e2a8dc05a92
macbookair <kivydevelopment@gmail.com>
2022-08-27 22:25:39 +0300

    The `StencilWidget` class is marked as deprecated

--- kivymd/uix/templates/stencilwidget/stencilwidget.py
+++ kivymd/uix/templates/stencilwidget/stencilwidget.py
@@ -2,115 +2,33 @@
 Templates/StencilWidget
 =======================
 
-.. versionadded:: 1.0.0
+.. deprecated:: 1.1.0
 
 Base class for controlling the stencil instructions of the widget.
 
-.. note:: See `Stencil instructions
-    <https://kivy.org/doc/stable/api-kivy.graphics.stencil_instructions.html>`_
-    for more information.
-
-Kivy
-----
-
-.. code-block:: python
-
-    from kivy.lang import Builder
-    from kivy.app import App
-
-    KV = '''
-    Carousel:
-
-        Button:
-            size_hint: .9, .8
-            pos_hint: {"center_x": .5, "center_y": .5}
-
-            canvas.before:
-                StencilPush
-                RoundedRectangle:
-                    pos: root.pos
-                    size: root.size
-                StencilUse
-            canvas.after:
-                StencilUnUse
-                RoundedRectangle:
-                    pos: root.pos
-                    size: root.size
-                StencilPop
-    '''
-
-
-    class Test(App):
-        def build(self):
-            return Builder.load_string(KV)
-
-
-    Test().run()
-
-KivyMD
-------
-
-.. code-block:: python
-
-    from kivy.lang import Builder
-
-    from kivymd.app import MDApp
-    from kivymd.uix.templates import StencilWidget
-    from kivymd.uix.fitimage import FitImage
-
-    KV = '''
-    MDCarousel:
-
-        StencilImage:
-            size_hint: .9, .8
-            pos_hint: {"center_x": .5, "center_y": .5}
-            source: "image.png"
-    '''
-
-
-    class StencilImage(FitImage, StencilWidget):
-        pass
-
-
-    class Test(MDApp):
-        def build(self):
-            return Builder.load_string(KV)
-
-
-    Test().run()
+.. note:: `StencilWidget` class has been deprecated. Please use
+    `StencilBehavior <https://kivymd.readthedocs.io/en/latest/behaviors/stencil/>`_
+    class instead.
 """
 
 __all__ = ("StencilWidget",)
 
-import os
+from kivy import Logger
 
-from kivy.lang import Builder
-from kivy.properties import VariableListProperty
-
-from kivymd import uix_path
-
-with open(
-    os.path.join(uix_path, "templates", "stencilwidget", "stencilwidget.kv"),
-    encoding="utf-8",
-) as kv_file:
-    Builder.load_string(kv_file.read())
+from kivymd.uix.behaviors import StencilBehavior
 
 
-class StencilWidget:
-    """Base class for controlling the stencil instructions of the widget"""
-
-    radius = VariableListProperty([0], length=4)
+class StencilWidget(StencilBehavior):
     """
-    Canvas radius.
-
-    .. versionadded:: 1.0.0
-
-    .. code-block:: python
-
-        # Top left corner slice.
-        MDWidget:
-            radius: [25, 0, 0, 0]
-
-    :attr:`radius` is an :class:`~kivy.properties.VariableListProperty`
-    and defaults to `[0, 0, 0, 0]`.
+    .. deprecated:: 1.1.0
+        Use :class:`~kivymd.uix.behaviors.scale_behavior.StencilBehavior`
+        class instead.
     """
+
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        Logger.warning(
+            "KivyMD: "
+            "The `StencilWidget` class has been deprecated. "
+            "Use the `StencilBehavior` class instead."
+        )

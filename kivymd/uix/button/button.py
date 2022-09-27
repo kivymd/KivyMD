9bfbb079285c6a66f1d2d1989a0bb89941f2f047
Yuri Ivanov <kivydevelopment@gmail.com>
2022-09-28 00:29:45 +0300

    Add doc string

--- kivymd/uix/button/button.py
+++ kivymd/uix/button/button.py
@@ -519,6 +519,119 @@ You can set your color values for background, text of buttons etc:
 
 .. image:: https://github.com/HeaTTheatR/KivyMD-data/raw/master/gallery/kivymddoc/MDFloatingActionButtonSpeedDial-hint-color.png
     :align: center
+
+Binds to individual buttons
+---------------------------
+
+.. tabs::
+
+    .. tab:: Declarative KV style
+
+        .. code-block:: python
+
+            from kivy.lang import Builder
+            from kivy.properties import DictProperty
+
+            from kivymd.app import MDApp
+
+            KV = '''
+            MDScreen:
+
+                MDFloatingActionButtonSpeedDial:
+                    id: speed_dial
+                    data: app.data
+                    root_button_anim: True
+                    hint_animation: True
+            '''
+
+
+            class Example(MDApp):
+                data = DictProperty()
+
+                def build(self):
+                    self.theme_cls.theme_style = "Dark"
+                    self.theme_cls.primary_palette = "Orange"
+                    self.data = {
+                        'Python': 'language-python',
+                        'JS': [
+                            'language-javascript',
+                            "on_press", lambda x: print("pressed JS"),
+                            "on_release", lambda x: print(
+                                "stack_buttons",
+                                self.root.ids.speed_dial.stack_buttons
+                            )
+                        ],
+                        'PHP': [
+                            'language-php',
+                            "on_press", lambda x: print("pressed PHP"),
+                            "on_release", self.callback
+                        ],
+                        'C++': [
+                            'language-cpp',
+                            "on_press", lambda x: print("pressed C++"),
+                            "on_release", lambda x: self.callback()
+                        ],
+                    }
+                    return Builder.load_string(KV)
+
+                def callback(self, *args):
+                    print(args)
+
+
+            Example().run()
+
+    .. tab:: Declarative python style
+
+        .. code-block:: python
+
+            from kivymd.app import MDApp
+            from kivymd.uix.button import MDFloatingActionButtonSpeedDial
+            from kivymd.uix.screen import MDScreen
+
+
+            class Example(MDApp):
+                def build(self):
+                    self.theme_cls.theme_style = "Dark"
+                    self.theme_cls.primary_palette = "Orange"
+                    return (
+                        MDScreen(
+                            MDFloatingActionButtonSpeedDial(
+                                id="speed_dial",
+                                hint_animation=True,
+                                root_button_anim=True,
+                            )
+                        )
+                    )
+
+                def on_start(self):
+                    data = {
+                        "Python": "language-python",
+                        "JS": [
+                            "language-javascript",
+                            "on_press", lambda x: print("pressed JS"),
+                            "on_release", lambda x: print(
+                                "stack_buttons",
+                                self.root.ids.speed_dial.stack_buttons
+                            )
+                        ],
+                        "PHP": [
+                            "language-php",
+                            "on_press", lambda x: print("pressed PHP"),
+                            "on_release", self.callback
+                        ],
+                        "C++": [
+                            "language-cpp",
+                            "on_press", lambda x: print("pressed C++"),
+                            "on_release", lambda x: self.callback()
+                        ],
+                    }
+                    self.root.ids.speed_dial.data = data
+
+                def callback(self, *args):
+                    print(args)
+
+
+            Example().run()
 """
 
 from __future__ import annotations

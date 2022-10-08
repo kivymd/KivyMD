c8cfd22c0161596b20b8b843aea6936396ccf666
Yuri Ivanov <kivydevelopment@gmail.com>
2022-10-09 00:09:57 +0300

    Fix python version

--- kivymd/tests/test_create_project.py
+++ kivymd/tests/test_create_project.py
@@ -2,7 +2,7 @@ def test_create_project():
     import os
 
     os.system(
-        f"python3.10 -m kivymd.tools.patterns.create_project "
+        f"python -m kivymd.tools.patterns.create_project "
         f"MVC "
         f"{os.path.expanduser('~')} "
         f"TestProject "

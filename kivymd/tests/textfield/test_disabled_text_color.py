01ab97f8b25be5d5ea09123f91c15046516f220d
Yuri Ivanov <kivydevelopment@gmail.com>
2022-11-20 00:54:16 +0300

    Added a test
    Added a test to check the color of the text field when disabled.

---
+++ kivymd/tests/textfield/test_disabled_text_color.py
@@ -0,0 +1,44 @@
+from kivy.clock import Clock
+from kivy.lang import Builder
+
+from kivymd.app import MDApp
+
+KV = """
+MDScreen:
+
+    MDTextField:
+        id: field
+        text: "Text"
+        hint_text: "Hint text"
+        helper_text: "Helper text"
+        helper_text_mode: "persistent"
+        icon_right: "gmail"
+        size_hint_x: .5
+        max_text_length: 3
+        pos_hint: {"center_x": .5, "center_y": .5}
+        disabled: True
+"""
+
+
+class TestDisabledTextColor(MDApp):
+    def build(self):
+        return Builder.load_string(KV)
+
+    def check_colors(self, *args):
+        for instruction in self.root.ids.field.canvas.before.children:
+            if instruction.group in [
+                "hint-text-color",
+                "max-length-color",
+                "right-left-icons-color",
+                "helper-text-color",
+            ]:
+                assert (
+                    instruction.rgba == self.theme_cls.disabled_hint_text_color
+                )
+        self.stop()
+
+    def on_start(self):
+        Clock.schedule_once(self.check_colors, 2)
+
+
+TestDisabledTextColor().run()

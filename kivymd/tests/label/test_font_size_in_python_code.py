ae8be450a989fcc84d97649588bb6c88c0be7c01
Yuri Ivanov <kivydevelopment@gmail.com>
2022-12-25 07:59:07 +0300

    Add test
    Test task:
    check the size of the custom font that was installed in the Python code.

---
+++ kivymd/tests/label/test_font_size_in_python_code.py
@@ -0,0 +1,35 @@
+# Test for https://github.com/kivymd/KivyMD/issues/1435:
+#
+# Test task:
+#
+# - check the size of the custom font that was installed in the Python code;
+
+from kivy.clock import Clock
+
+from kivymd.app import MDApp
+from kivymd.uix.boxlayout import MDBoxLayout
+from kivymd.uix.label import MDLabel
+
+
+class TestFontSizeInPythonCode(MDApp):
+    def check_font_size(self, *args):
+        assert self.root.ids.label.font_size == 40
+        self.stop()
+
+    def build(self):
+        return (
+            MDBoxLayout(
+                MDLabel(
+                    id="label",
+                    text="MDLabel",
+                    font_style="H5",
+                    font_size=40,
+                ),
+            )
+        )
+
+    def on_start(self):
+        Clock.schedule_once(self.check_font_size, 1.2)
+
+
+TestFontSizeInPythonCode().run()

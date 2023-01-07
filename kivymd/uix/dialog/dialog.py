1460274011358b57627950e7c1efec0c33e72cfa
Yuri Ivanov <kivydevelopment@gmail.com>
2023-01-07 12:09:53 +0300

    Fix doc string

--- kivymd/uix/dialog/dialog.py
+++ kivymd/uix/dialog/dialog.py
@@ -296,22 +296,22 @@ class MDDialog(BaseDialog):
         class Example(MDApp):
             dialog = None
 
-        def build(self):
-            self.theme_cls.theme_style = "Dark"
-            self.theme_cls.primary_palette = "Orange"
-            return Builder.load_string(KV)
+            def build(self):
+                self.theme_cls.theme_style = "Dark"
+                self.theme_cls.primary_palette = "Orange"
+                return Builder.load_string(KV)
 
-        def show_simple_dialog(self):
-            if not self.dialog:
-                self.dialog = MDDialog(
-                    title="Set backup account",
-                    type="simple",
-                    items=[
-                        Item(text="user01@gmail.com", source="kivymd/images/logo/kivymd-icon-128.png"),
-                        Item(text="user02@gmail.com", source="data/logo/kivy-icon-128.png"),
-                    ],
-                )
-            self.dialog.open()
+            def show_simple_dialog(self):
+                if not self.dialog:
+                    self.dialog = MDDialog(
+                        title="Set backup account",
+                        type="simple",
+                        items=[
+                            Item(text="user01@gmail.com", source="kivymd/images/logo/kivymd-icon-128.png"),
+                            Item(text="user02@gmail.com", source="data/logo/kivy-icon-128.png"),
+                        ],
+                    )
+                self.dialog.open()
 
 
         Example().run()

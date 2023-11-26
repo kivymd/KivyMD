d258f4d7ace078615f04a2e3d40bc71567d7668f
Иванов Юрий <kivydevelopment@gmail.com>
2023-11-26 16:54:55 +0300

    2.0.0 - Material Design 3
    Fix doc strings.

--- kivymd/uix/menu/menu.py
+++ kivymd/uix/menu/menu.py
@@ -405,8 +405,8 @@ Center position
 .. code-block:: python
 
     from kivy.lang import Builder
-    from kivymd.uix.menu import MDDropdownMenu
 
+    from kivymd.uix.menu import MDDropdownMenu
     from kivymd.app import MDApp
 
     KV = '''
@@ -415,7 +415,7 @@ Center position
 
         MDDropDownItem:
             pos_hint: {"center_x": .5, "center_y": .5}
-            on_release: app.open_menu(self)
+            on_release: app.open_drop_item_menu(self)
 
             MDDropDownItemText:
                 id: drop_text
@@ -423,23 +423,25 @@ Center position
     '''
 
 
-    class Example(MDApp):
-        menu = None
+    class Example(MDApp, CommonApp):
+        drop_item_menu: MDDropdownMenu = None
 
-        def open_menu(self, item):
+        def open_drop_item_menu(self, item):
             menu_items = [
                 {
                     "text": f"{i}",
                     "on_release": lambda x=f"Item {i}": self.menu_callback(x),
                 } for i in range(5)
             ]
-            self.menu = MDDropdownMenu(
-                caller=item, items=menu_items, position="center"
-            ).open()
+            if not self.drop_item_menu:
+                self.drop_item_menu = MDDropdownMenu(
+                    caller=item, items=menu_items, position="center"
+                )
+                self.drop_item_menu.open()
 
         def menu_callback(self, text_item):
             self.root.ids.drop_text.text = text_item
-            self.menu.dismiss()
+            self.drop_item_menu.dismiss()
 
         def build(self):
             return Builder.load_string(KV)

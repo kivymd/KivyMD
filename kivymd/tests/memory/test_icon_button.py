d2974777cd5be068b6430913c9676494abce2d3c
Иванов Юрий <kivydevelopment@gmail.com>
2023-11-27 13:41:37 +0300

    2.0.0 - Material Design 3
    Add new tests.

---
+++ kivymd/tests/memory/test_icon_button.py
@@ -0,0 +1,42 @@
+from kivy.clock import Clock
+
+from kivymd.app import MDApp
+from kivymd.uix.button import MDIconButton
+from kivymd.uix.screen import MDScreen
+
+len_callbacks = 0
+
+
+class MyScreen(MDScreen):
+    def remove_widget(self, *args, **kwargs) -> None:
+        global len_callbacks
+
+        super().remove_widget(*args, **kwargs)
+        len_callbacks = len(
+            self.theme_cls.get_property_observers("theme_style")
+        )
+
+
+class TestButtonMemoryLeak(MDApp):
+    counter = 0
+    previous_len_callbacks = 0
+
+    def build(self):
+        Clock.schedule_interval(self.add_items, 0.5)
+        return MyScreen()
+
+    def add_items(self, *args):
+        if len_callbacks:
+            self.previous_len_callbacks = len_callbacks
+
+        self.counter += 1
+        self.root.clear_widgets()
+        self.root.add_widget(MDIconButton(icon="plus"))
+
+        if self.counter > 10:
+            Clock.unschedule(self.add_items)
+            assert len_callbacks == self.previous_len_callbacks
+            self.stop()
+
+
+TestButtonMemoryLeak().run()

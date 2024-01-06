f9b1bc099a1764853cab463a71ad226bd3118224
Иванов Юрий <kivydevelopment@gmail.com>
2024-01-06 06:41:55 +0300

    2.0.0 - Material Design 3
    - Added supporting Material Design 3
    - Added new color schemes and effects

--- kivymd/uix/expansionpanel/__init__.py
+++ kivymd/uix/expansionpanel/__init__.py
@@ -1,8 +1,4 @@
 # NOQA F401
 from .expansionpanel import (
-    MDExpansionPanel,
-    MDExpansionPanelLabel,
-    MDExpansionPanelOneLine,
-    MDExpansionPanelThreeLine,
-    MDExpansionPanelTwoLine,
+    MDExpansionPanel, MDExpansionPanelContent, MDExpansionPanelHeader
 )

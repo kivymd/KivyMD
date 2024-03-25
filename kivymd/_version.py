2c24739dc8a0549d6854e6953bab3519bdca211a
Bageni Gilbert <135163732+bagenigilbert@users.noreply.github.com>
2024-03-25 14:25:27 +0000

    Fixed issues and updated examples in Dynamic color module

--- kivymd/_version.py
+++ kivymd/_version.py
@@ -1,5 +1,5 @@
 release = False
 __version__ = "2.0.1.dev0"
-__hash__ = "f7bde69707ac708a758a02d89f14997ee468d1ee"
-__short_hash__ = "f7bde69"
-__date__ = "2024-02-27"
+__hash__ = "4e9fdba5263b110010822c71e50c00df280405dc"
+__short_hash__ = "4e9fdba"
+__date__ = "2024-03-25"

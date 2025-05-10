81c482cc61c727b8b940265c99b65ee83c396a74
Иванов Юрий <kivydevelopment@gmail.com>
2025-05-10 07:34:48 +0300

    Update doc strings

--- kivymd/uix/widget.py
+++ kivymd/uix/widget.py
@@ -8,30 +8,121 @@ with some widget properties. For example:
 Widget
 ------
 
-.. code-block:: kv
+.. tabs::
 
-    Widget:
-        size_hint: .5, None
-        height: self.width
+    .. tab:: KV
 
-        canvas:
-            Color:
-                rgba: app.theme_cls.primaryColor
-            RoundedRectangle:
-                pos: self.pos
-                size: self.size
-                radius: [self.height / 2,]
+        .. code-block:: kv
+
+            Widget:
+                canvas:
+                    Color:
+                        rgba: app.theme_cls.primaryColor
+                    Rectangle:
+                        pos: self.pos
+                        size: self.size
+
+    .. tab:: Python
+
+        .. code-block:: python
+
+            from kivy.uix.widget import Widget
+            from kivy.graphics import Color, Rectangle
+            from kivy.app import App
+
+            class MyApp(App):
+                def build(self):
+                    widget = Widget()
+
+                    with widget.canvas:
+                        Color(*self.theme_cls.primary_color)
+                        self.rect = Rectangle(pos=layout.pos, size=layout.size)
+
+                    return widget
+
+            MyApp().run()
 
 MDWidget
 --------
 
+.. tabs::
+
+    .. tab:: Imperative python style with KV
+
+        .. code-block:: kv
+
+            MDWidget:
+                md_bg_color: app.theme_cls.primaryColor
+
+    .. tab:: Declarative python style
+
+        .. code-block:: python
+
+            from kivymd.uix.widget import MDWidget
+            from kivymd.app import MDApp
+
+            class MyApp(App):
+                def build(self):
+                    return MDWidget(
+                        md_bg_color=self.theme_cls.primaryColor
+                    )
+
+            MyApp().run()
+
+Available options are:
+----------------------
+
+- adaptive_height_
+- adaptive_width_
+- adaptive_size_
+
+.. adaptive_height:
+
+adaptive_height
+---------------
+
 .. code-block:: kv
 
-    MDWidget:
-        size_hint: .5, None
-        height: self.width
-        radius: self.height / 2
-        md_bg_color: app.theme_cls.primaryColor
+    adaptive_height: True
+
+Equivalent
+
+.. code-block:: kv
+
+    size_hint_y: None
+    height: self.minimum_height
+
+.. adaptive_width:
+
+adaptive_width
+--------------
+
+.. code-block:: kv
+
+    adaptive_width: True
+
+Equivalent
+
+.. code-block:: kv
+
+    size_hint_x: None
+    height: self.minimum_width
+
+.. adaptive_size:
+
+adaptive_size
+-------------
+
+.. code-block:: kv
+
+    adaptive_size: True
+
+Equivalent
+
+.. code-block:: kv
+
+    size_hint: None, None
+    size: self.minimum_size
 """
 
 __all__ = ("MDWidget",)

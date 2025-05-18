b67532bebe4221203f75c439e5cba82ee36431ef
Иванов Юрий <kivydevelopment@gmail.com>
2025-05-18 14:31:44 +0300

    Update doc strings

--- kivymd/uix/refreshlayout/refreshlayout.py
+++ kivymd/uix/refreshlayout/refreshlayout.py
@@ -5,105 +5,211 @@ Components/RefreshLayout
 Example
 -------
 
-.. code-block:: python
+.. tabs::
 
-    from kivy.clock import Clock
-    from kivy.lang import Builder
-    from kivy.factory import Factory
-    from kivy.properties import StringProperty
+    .. tab:: Declarative Python style with KV
 
-    from kivymd.app import MDApp
-    from kivymd.uix.button import MDIconButton
-    from kivymd.icon_definitions import md_icons
-    from kivymd.uix.list import ILeftBodyTouch, OneLineIconListItem
-    from kivymd.theming import ThemeManager
-    from kivymd.utils import asynckivy
+        .. code-block:: python
 
-    Builder.load_string('''
-    <ItemForList>
-        text: root.text
+            from kivy.clock import Clock
+            from kivy.lang import Builder
 
-        IconLeftSampleWidget:
-            icon: root.icon
+            from kivymd.app import MDApp
+            from kivymd.icon_definitions import md_icons
+            from kivymd.uix.list import (
+                MDListItem, MDListItemHeadlineText, MDListItemTrailingIcon
+            )
 
+            import asynckivy
 
-    <Example@MDFloatLayout>
+            KV = '''
+            MDScreen:
+                md_bg_color: self.theme_cls.backgroundColor
 
-        MDBoxLayout:
-            orientation: 'vertical'
+                MDBoxLayout:
+                    orientation: 'vertical'
 
-            MDTopAppBar:
-                title: app.title
-                md_bg_color: app.theme_cls.primary_color
-                background_palette: 'Primary'
-                elevation: 4
-                left_action_items: [['menu', lambda x: x]]
+                    MDTopAppBar:
 
-            MDScrollViewRefreshLayout:
-                id: refresh_layout
-                refresh_callback: app.refresh_callback
-                root_layout: root
-                spinner_color: "brown"
-                circle_color: "white"
+                        MDTopAppBarLeadingButtonContainer:
 
-                MDGridLayout:
-                    id: box
-                    adaptive_height: True
-                    cols: 1
-    ''')
+                            MDActionTopAppBarButton:
+                                icon: 'menu'
 
+                        MDTopAppBarTitle:
+                            text: 'Example Refresh Layout'
 
-    class IconLeftSampleWidget(ILeftBodyTouch, MDIconButton):
-        pass
+                    MDScrollViewRefreshLayout:
+                        id: refresh_layout
+                        refresh_callback: app.refresh_callback
+                        root_layout: root
+                        spinner_color: "brown"
+                        circle_color: "white"
 
-
-    class ItemForList(OneLineIconListItem):
-        icon = StringProperty()
-
-
-    class Example(MDApp):
-        title = 'Example Refresh Layout'
-        screen = None
-        x = 0
-        y = 15
-
-        def build(self):
-            self.theme_cls.theme_style = "Dark"
-            self.theme_cls.primary_palette = "Orange"
-            self.screen = Factory.Example()
-            self.set_list()
-
-            return self.screen
-
-        def set_list(self):
-            async def set_list():
-                names_icons_list = list(md_icons.keys())[self.x:self.y]
-                for name_icon in names_icons_list:
-                    await asynckivy.sleep(0)
-                    self.screen.ids.box.add_widget(
-                        ItemForList(icon=name_icon, text=name_icon))
-            asynckivy.start(set_list())
-
-        def refresh_callback(self, *args):
-            '''
-            A method that updates the state of your application
-            while the spinner remains on the screen.
+                        MDGridLayout:
+                            id: box
+                            adaptive_height: True
+                            cols: 1
             '''
 
-            def refresh_callback(interval):
-                self.screen.ids.box.clear_widgets()
-                if self.x == 0:
-                    self.x, self.y = 15, 30
-                else:
-                    self.x, self.y = 0, 15
-                self.set_list()
-                self.screen.ids.refresh_layout.refresh_done()
-                self.tick = 0
 
-            Clock.schedule_once(refresh_callback, 1)
+            class Example(MDApp):
+                x = 0
+                y = 15
+
+                def build(self):
+                    self.theme_cls.theme_style = "Dark"
+                    self.theme_cls.primary_palette = "Orange"
+                    return Builder.load_string(KV)
+
+                def on_start(self):
+                    self.set_list()
+
+                def set_list(self):
+                    async def set_list():
+                        names_icons_list = list(md_icons.keys())[self.x:self.y]
+                        for name_icon in names_icons_list:
+                            await asynckivy.sleep(0)
+                            self.root.ids.box.add_widget(
+                                MDListItem(
+                                    MDListItemHeadlineText(
+                                        text=name_icon
+                                    ),
+                                    MDListItemTrailingIcon(
+                                        icon=name_icon
+                                    )
+                                )
+                            )
+
+                    asynckivy.start(set_list())
+
+                def refresh_callback(self, *args):
+                    '''
+                    A method that updates the state of your application
+                    while the spinner remains on the screen.
+                    '''
+
+                    def refresh_callback(interval):
+                        self.root.ids.box.clear_widgets()
+                        if self.x == 0:
+                            self.x, self.y = 15, 30
+                        else:
+                            self.x, self.y = 0, 15
+                        self.set_list()
+                        self.root.ids.refresh_layout.refresh_done()
+                        self.tick = 0
+
+                    Clock.schedule_once(refresh_callback, 1)
 
 
-    Example().run()
+            Example().run()
+
+    .. tab:: Declarative python style
+
+        .. code-block:: python
+
+            from kivy.clock import Clock
+
+            from kivymd.app import MDApp
+            from kivymd.uix.boxlayout import MDBoxLayout
+            from kivymd.icon_definitions import md_icons
+            from kivymd.uix.floatlayout import MDFloatLayout
+            from kivymd.uix.gridlayout import MDGridLayout
+            from kivymd.uix.refreshlayout import MDScrollViewRefreshLayout
+            from kivymd.uix.list import (
+                MDListItem, MDListItemHeadlineText, MDListItemTrailingIcon
+            )
+            from kivymd.uix.appbar import (
+                MDTopAppBar,
+                MDTopAppBarLeadingButtonContainer,
+                MDActionTopAppBarButton,
+                MDTopAppBarTitle,
+            )
+
+            import asynckivy
+
+
+            class Example(MDApp):
+                x = 0
+                y = 15
+
+                def build(self):
+                    self.theme_cls.theme_style = "Dark"
+                    self.theme_cls.primary_palette = "Orange"
+                    return (
+                        MDFloatLayout(
+                            MDBoxLayout(
+                                MDTopAppBar(
+                                    MDTopAppBarLeadingButtonContainer(
+                                        MDActionTopAppBarButton(
+                                            icon='menu'
+                                        )
+                                    ),
+                                    MDTopAppBarTitle(
+                                        text='Example Refresh Layout'
+                                    )
+                                ),
+                                MDScrollViewRefreshLayout(
+                                    MDGridLayout(
+                                        id="box",
+                                        adaptive_height=True,
+                                        cols=1,
+                                    ),
+                                    id="refresh_layout",
+                                    refresh_callback=self.refresh_callback,
+                                    spinner_color="red",
+                                    circle_color="white",
+                                ),
+                                orientation='vertical'
+                            ),
+                            md_bg_color=self.theme_cls.backgroundColor
+                        )
+                    )
+
+                def on_start(self):
+                    self.root.get_ids().refresh_layout.root_layout = self.root
+                    self.set_list()
+
+                def set_list(self):
+                    async def set_list():
+                        names_icons_list = list(md_icons.keys())[self.x:self.y]
+                        for name_icon in names_icons_list:
+                            await asynckivy.sleep(0)
+                            self.root.get_ids().box.add_widget(
+                                MDListItem(
+                                    MDListItemHeadlineText(
+                                        text=name_icon
+                                    ),
+                                    MDListItemTrailingIcon(
+                                        icon=name_icon
+                                    )
+                                )
+                            )
+                    asynckivy.start(set_list())
+
+                def refresh_callback(self, *args):
+                    '''
+                    A method that updates the state of your application
+                    while the spinner remains on the screen.
+                    '''
+
+                    def refresh_callback(interval):
+                        self.root.get_ids().box.clear_widgets()
+                        if self.x == 0:
+                            self.x, self.y = 15, 30
+                        else:
+                            self.x, self.y = 0, 15
+                        self.set_list()
+                        self.root.get_ids().refresh_layout.refresh_done()
+                        self.tick = 0
+
+                    Clock.schedule_once(refresh_callback, 1)
+
+
+            Example().run()
+
+.. image:: https://github.com/HeaTTheatR/KivyMD-data/raw/master/gallery/kivymddoc/refresh-layout-example.gif
+    :align: center
 """
 
 __all__ = ("MDScrollViewRefreshLayout",)
@@ -161,14 +267,13 @@ class _RefreshScrollEffect(DampedScrollEffect):
             return False
 
 
-class MDScrollViewRefreshLayout(ThemableBehavior, MDScrollView):
+class MDScrollViewRefreshLayout(MDScrollView, ThemableBehavior):
     """
     Refresh layout class.
 
     For more information, see in the
-    :class:`~kivymd.theming.ThemableBehavior` and
-    :class:`~kivymd.uix.scrollview.MDScrollView`
-    class documentation.
+    :class:`~kivymd.uix.scrollview.MDScrollView` and
+    :class:`~kivymd.theming.ThemableBehavior` class documentation.
     """
 
     root_layout = ObjectProperty()
@@ -251,7 +356,7 @@ class MDScrollViewRefreshLayout(ThemableBehavior, MDScrollView):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         if not self.circle_color:
-            self.circle_color = self.theme_cls.primary_dark
+            self.circle_color = self.theme_cls.primaryColor
         self.effect_cls = _RefreshScrollEffect
         self._work_spinner = False
         self._did_overscroll = False
@@ -304,7 +409,7 @@ class RefreshSpinner(ThemableBehavior, FloatLayout):
     def start_anim_spinner(self) -> None:
         spinner = self.ids.body_spinner
         Animation(
-            y=spinner.y - self.theme_cls.standard_increment * 2 + dp(10),
+            y=spinner.y - dp(48) * 2 + dp(10),
             d=self.show_duration,
             t=self.show_transition,
         ).start(spinner)

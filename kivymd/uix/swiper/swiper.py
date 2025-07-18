1238ebb2b8bc946dc1c079d3c0b4efffa81ea58f
Иванов Юрий <kivydevelopment@gmail.com>
2025-07-18 22:20:47 +0300

    Update doc string

--- kivymd/uix/swiper/swiper.py
+++ kivymd/uix/swiper/swiper.py
@@ -8,60 +8,127 @@ Components/Swiper
 Usage
 =====
 
-.. code-block:: kv
+.. tabs::
 
-    MDSwiper:
+    .. tab:: Declarative KV style
 
-        MDSwiperItem:
+        .. code-block:: kv
 
-        MDSwiperItem:
+            MDSwiper:
 
-        MDSwiperItem:
+                MDSwiperItem:
+
+                MDSwiperItem:
+
+                MDSwiperItem:
+
+    .. tab:: Declarative Python style
+
+        .. code-block:: python
+
+                MDSwiper(
+                    MDSwiperItem(),
+                    MDSwiperItem(),
+                    MDSwiperItem(),
+                )
 
 Example
 =======
 
-.. code-block:: python
+.. tabs::
 
-    from kivy.lang.builder import Builder
+    .. tab:: Declarative KV style
 
-    from kivymd.app import MDApp
+        .. code-block:: python
 
-    kv = '''
-    <MySwiper@MDSwiperItem>
+            from kivy.lang.builder import Builder
 
-        FitImage:
-            source: "bg.jpg"
-            radius: [dp(20),]
+            from kivymd.app import MDApp
 
+            kv = '''
+            <MySwiper@MDSwiperItem>
 
-    MDScreen:
-        md_bg_color: self.theme_cls.backgroundColor
-
-        MDSwiper:
-            size_hint_y: None
-            height: root.height - dp(40)
-            y: root.height - self.height - dp(20)
-
-            MySwiper:
-
-            MySwiper:
-
-            MySwiper:
-
-            MySwiper:
-
-            MySwiper:
-    '''
+                FitImage:
+                    source: "bg.jpg"
+                    radius: [dp(20),]
 
 
-    class Main(MDApp):
-        def build(self):
-            self.theme_cls.theme_style = "Dark"
-            return Builder.load_string(kv)
+            MDScreen:
+                md_bg_color: self.theme_cls.backgroundColor
+
+                MDSwiper:
+                    size_hint_y: None
+                    height: root.height - dp(40)
+                    y: root.height - self.height - dp(20)
+
+                    MySwiper:
+
+                    MySwiper:
+
+                    MySwiper:
+
+                    MySwiper:
+
+                    MySwiper:
+            '''
 
 
-    Main().run()
+            class Main(MDApp):
+                def build(self):
+                    self.theme_cls.theme_style = "Dark"
+                    return Builder.load_string(kv)
+
+
+            Main().run()
+
+    .. tab:: Declarative Python style
+
+        .. code-block:: python
+
+            from kivymd.material_resources import dp
+
+            from kivymd.app import MDApp
+            from kivymd.uix.fitimage import FitImage
+            from kivymd.uix.screen import MDScreen
+            from kivymd.uix.swiper import MDSwiper, MDSwiperItem
+
+
+            class MySwiper(MDSwiperItem):
+                def __init__(self, *args, **kwargs):
+                    super().__init__(*args, **kwargs)
+                    self.widgets = [
+                        FitImage(
+                            source="bg.jpg",
+                            radius=[dp(20), ],
+                        )
+                    ]
+
+
+            class Main(MDApp):
+                def on_start(self):
+                    swiper = self.root.get_ids().swiper
+                    swiper.height = (self.root.height - dp(40))
+                    swiper.y = (self.root.height - swiper.height - dp(20))
+
+                def build(self):
+                    self.theme_cls.theme_style = "Dark"
+                    return (
+                        MDScreen(
+                            MDSwiper(
+                                MySwiper(),
+                                MySwiper(),
+                                MySwiper(),
+                                MySwiper(),
+                                MySwiper(),
+                                size_hint_y=None,
+                                id="swiper",
+                            ),
+                            md_bg_color=self.theme_cls.backgroundColor,
+                        )
+                    )
+
+
+            Main().run()
 
 .. image:: https://github.com/HeaTTheatR/KivyMD-data/raw/master/gallery/kivymddoc/swiper-example.gif
     :align: center

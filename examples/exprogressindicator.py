868f36d20e92cb9c62585ae79685b90717836fc2
HeaTTheatR <kivydevelopment@gmail.com>
2026-07-15 16:44:41 +0500

    Updated KV code structure for better readability

--- examples/exprogressindicator.py
+++ examples/exprogressindicator.py
@@ -1,4 +1,3 @@
-from kivy.animation import Animation
 from kivy.clock import Clock
 from kivy.lang import Builder
 from kivy.properties import BooleanProperty, StringProperty
@@ -14,8 +13,7 @@ KV = """
 
     MDCheckbox:
         on_active: root.dispatch("on_active", self.active)
-        active: 
-            root.active
+        active: root.active
 
     MDLabel:
         text: root.text
@@ -37,17 +35,18 @@ MDScreen:
     BoxLayout:
         spacing: "24dp"
 
- 
         BoxLayout:
-            spacing:dp(10)
-            orientation:"vertical"
-            padding:[dp(50), dp(80), 0, dp(20)]
+            spacing: dp(10)
+            orientation: "vertical"
+            padding: [dp(50), dp(80), 0, dp(20)]
 
             BoxLayout:
-                orientation:"vertical"
+                orientation: "vertical"
+
                 MDLabel:
                     text: "Progress (0-100)"
                     adaptive_height: True
+
                 MDSlider:
                     min:0
                     max:100 
@@ -57,126 +56,146 @@ MDScreen:
                         linear_indicator_horizontal.value = self.value
                         linear_indicator_vertical.value = self.value
                         circular_indicator.value = self.value
+
                     MDSliderHandle:
+
                     MDSliderValueLabel:
 
-
             BoxLayout:
-                orientation:"vertical"
+                orientation: "vertical"
+
                 MDLabel:
                     text: "Amplitude (0 - 30) dp"
                     adaptive_height: True
+
                 MDSlider:
-                    min:dp(0)
-                    max:dp(30)
+                    min: dp(0)
+                    max: dp(30)
                     step: 1
-                    value:dp(3)
+                    value: dp(3)
                     on_value:
                         linear_indicator_horizontal.amplitude = self.value
                         linear_indicator_vertical.amplitude = self.value
                         circular_indicator.amplitude = self.value
+
                     MDSliderHandle:
  
             BoxLayout:
-                orientation:"vertical"
+                orientation: "vertical"
+
                 MDLabel:
                     text: "Wavelenght (0 - 100) dp"
                     adaptive_height: True
+
                 MDSlider:
-                    min:dp(0)
-                    max:dp(100)
+                    min: dp(0)
+                    max: dp(100)
                     step: 2
-                    value:dp(8)
+                    value: dp(8)
                     on_value:
                         linear_indicator_horizontal.wave_length = self.value
                         linear_indicator_vertical.wave_length = self.value
                         circular_indicator.wave_length = self.value
+
                     MDSliderHandle: 
 
             BoxLayout:
-                orientation:"vertical"
+                orientation: "vertical"
+
                 MDLabel:
                     text: "Wave speed (-50 - 50) dp/s"
                     adaptive_height: True
+
                 MDSlider:
-                    min:dp(-50)
-                    max:dp(50)
+                    min: dp(-50)
+                    max: dp(50)
                     step: 2
                     value:-dp(40)
                     on_value:
                         linear_indicator_horizontal.wave_speed = self.value
                         linear_indicator_vertical.wave_speed = self.value
                         circular_indicator.wave_speed = self.value
+
                     MDSliderHandle: 
 
             BoxLayout:
-                orientation:"vertical"
+                orientation: "vertical"
+
                 MDLabel:
                     text: "Thickness (4 - 20) dp"
                     adaptive_height: True
 
                 MDSlider:
-                    min:dp(4)
-                    max:dp(20)
+                    min: dp(4)
+                    max: dp(20)
                     step: 1
-                    value:dp(4) 
+                    value: dp(4) 
                     on_value:
                         linear_indicator_horizontal.thickness = self.value
                         linear_indicator_vertical.thickness = self.value
                         circular_indicator.thickness = self.value
+
                     MDSliderHandle:
+
                     MDSliderValueLabel:
 
             BoxLayout:
-                orientation:"vertical"
+                orientation: "vertical"
+
                 MDLabel:
                     text: "Circular indicator size (20 - 200)dp"
                     adaptive_height: True
 
                 MDSlider:
-                    min:dp(20)
-                    max:dp(240)
+                    min: dp(20)
+                    max: dp(240)
                     step: 2
-                    value:box.size[0]
+                    value: box.size[0]
                     on_value:
-                        box.size = [self.value]*2 
+                        box.size = [self.value] * 2 
+
                     MDSliderHandle:
+
                     MDSliderValueLabel:
 
             BoxLayout:
                 spacing: "12dp"
-                size_hint_y:None 
-                height:dp(50)
+                size_hint_y: None 
+                height: dp(50)
 
                 Widget:
+
                 MDLabel:
-                    text:"Determinate"
-                    adaptive_size:True
+                    text: "Determinate"
+                    adaptive_size: True
+
                 MDSwitch:
-                    active:True
+                    active: True
                     on_active:               
                         linear_indicator_horizontal.determinate = self.active
                         linear_indicator_vertical.determinate = self.active
                         circular_indicator.determinate = self.active
+
                 Widget:
 
         BoxLayout:
             spacing: "24dp"
-            orientation:"vertical"
-            padding:dp(20)
+            orientation: "vertical"
+            padding: dp(20)
 
             MDLabel:
                 id:fps
-                padding:[dp(10), 0]
-                halign:"center"
-                text:"FPS:"
-                adaptive_size:True 
+                padding: [dp(10), 0]
+                halign: "center"
+                text: "FPS: "
+                adaptive_size: True 
 
             AnchorLayout:
+
                 BoxLayout:
-                    id:box
+                    id: box
                     size_hint: None, None
-                    size: [dp(50)]*2
+                    size: [dp(50)] * 2
 
                     MDExCircularProgressIndicator:
                         id: circular_indicator
@@ -184,42 +203,70 @@ MDScreen:
 
             BoxLayout:
                 spacing: "12dp"
-                size_hint_y:None 
-                height:dp(50)
+                size_hint_y: None 
+                height: dp(50)
 
                 Widget:
+
                 MDLabel:
-                    text:"Retreat | Advanced"
-                    adaptive_size:True
+                    text: "Retreat | Advanced"
+                    adaptive_size: True
+
                 MDSwitch:
                     on_active:               
-                        circular_indicator.indeterminate_animator = "retreat" if self.active else "advanced"
+                        circular_indicator.indeterminate_animator = \
+                        "retreat" \
+                        if self.active else \
+                        "advanced"
+
                 Widget:
 
             MDExLinearProgressIndicator: 
-                color_array: [[1.0, 0.0, 0.0, 1.0], [1.0, 0.5, 0.0, 1.0], [1.0, 0.9, 0.0, 1.0], [0.0, 0.8, 0.3, 1.0], [0.0, 0.5, 1.0, 1.0], [0.6, 0.2, 0.8, 1.0]]
                 id: linear_indicator_horizontal
+                color_array: [ \
+                [1.0, 0.0, 0.0, 1.0], \
+                [1.0, 0.5, 0.0, 1.0], \
+                [1.0, 0.9, 0.0, 1.0], \
+                [0.0, 0.8, 0.3, 1.0], \
+                [0.0, 0.5, 1.0, 1.0], \
+                [0.6, 0.2, 0.8, 1.0] \
+                ]
 
             MDExLinearProgressIndicator:
                 id: linear_indicator_vertical
-                color_array: [[0.0, 0.4, 0.4, 1.0], [0.0, 0.7, 0.6, 1.0], [0.2, 0.8, 0.8, 1.0], [0.3, 0.5, 0.9, 1.0], [0.1, 0.2, 0.5, 1.0]]                
                 orientation: "vertical"
                 size_hint_y: None
                 height: self.width
+                color_array: [ \
+                [0.0, 0.4, 0.4, 1.0], \
+                [0.0, 0.7, 0.6, 1.0], \
+                [0.2, 0.8, 0.8, 1.0], \
+                [0.3, 0.5, 0.9, 1.0], \
+                [0.1, 0.2, 0.5, 1.0] \
+                ]                
 
             BoxLayout:
                 spacing: "12dp"
-                size_hint_y:None 
-                height:dp(50)
+                size_hint_y: None 
+                height: dp(50)
 
                 Widget:
+
                 MDLabel:
-                    text:"Disjoint | Contiguous"
-                    adaptive_size:True
+                    text: "Disjoint | Contiguous"
+                    adaptive_size: True
+
                 MDSwitch:
                     on_active:               
-                        linear_indicator_horizontal.indeterminate_animator = "discontinuous" if self.active else ""
-                        linear_indicator_vertical.indeterminate_animator = "discontinuous" if self.active else "" 
+                        linear_indicator_horizontal.indeterminate_animator = \
+                        "discontinuous" \
+                        if self.active else \
+                        ""
+                        linear_indicator_vertical.indeterminate_animator = \
+                        "discontinuous" \
+                        if self.active else \
+                        "" 
+
                 Widget:
 
 """
@@ -237,7 +284,6 @@ class SelectedItem(MDBoxLayout):
 
 
 class Example(MDApp, CommonApp):
-
     def build(self):
         self.theme_cls.primary_palette = "Olive"
         self.theme_cls.theme_style = "Dark"

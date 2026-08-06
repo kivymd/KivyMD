2d8a7b458897a01a400770e3bc10ebffd946b91d
HeaTTheatR <kivydevelopment@gmail.com>
2026-08-06 10:35:00 +0500

    KivyMD 2.0.0

--- kivymd/_version.py
+++ kivymd/_version.py
@@ -1,5 +1,5 @@
-release = False
-__version__ = "2.0.1.dev0"
+release = True
+__version__ = "2.0.0"
 __hash__ = "6c3480719c92c8e4ac47a7b0490b708ed0f2d4b8"
 __short_hash__ = "6c34807"
 __date__ = "2026-08-06"

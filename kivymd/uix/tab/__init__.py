5c214445fb12802f2fc38fef81760ff5120e78ab
HeaTTheatR <kivydevelopment@gmail.com>
2026-08-19 09:27:36 +0500

    feat: add iOS tab widgets

--- kivymd/uix/tab/__init__.py
+++ kivymd/uix/tab/__init__.py
@@ -1,4 +1,12 @@
 from .tab import (  # NOQA F401
+    IOSTabBarButton,
+    IOSTabBarHorizontal,
+    IOSTabBarIndicator,
+    IOSTabBarItem,
+    IOSTabBarItemIcon,
+    IOSTabBarItemText,
+    IOSTabBarLayout,
+    IOSTabBarVertical,
     MDTabsBadge,
     MDTabsCarousel,
     MDTabsItem,
